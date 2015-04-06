24bce9b2a1e716904a73d702e5bb822d2c668e7f
Justin Fear <jfear@ufl.edu>
2015-04-06 10:56:15 -0400

    Added logic to set xlim to 0 if no negative numbers.

--- distribution.py
+++ distribution.py
@@ -30,7 +30,7 @@ def getOptions():
     parser.add_argument("--fig2", dest="ofig2", action='store', required=True, help="Output figure name [html].")
     parser.add_argument("--debug", dest="debug", action='store_true', required=False, help="Add debugging log output.")
     args = parser.parse_args()
-#     args = parser.parse_args(['--input', '/home/jfear/sandbox/secim/data/ST000015_AN000032_v2.txt',
+#     args = parser.parse_args(['--input', '/home/jfear/sandbox/secim/data/ST000015_AN000032_v2.tsv',
 #                               '--design', '/home/jfear/sandbox/secim/data/ST000015_design_v2.tsv',
 #                               '--ID', 'Name',
 #                               '--group', 'treatment',
@@ -38,6 +38,17 @@ def getOptions():
 #                               '--fig2', '/home/jfear/sandbox/secim/data/test2.html',
 #                               '--debug'])
     return(args)
+
+
+def adjXCoord(ax):
+    """ Adjust xlim on plots with no negative number.
+
+    KDE of 0's can results in negative values, when data has no negative values
+    then change xaxis to start at 0.
+
+    """
+    xmin, xmax = ax.get_xlim()
+    ax.set_xlim(0, xmax)
 
 
 def pltByTrt(dat, ax):
@@ -52,15 +63,36 @@ def pltByTrt(dat, ax):
         samples = dat.design[dat.design[dat.group] == grp].index
         samp = dat.wide[samples]
         samp.plot(kind='kde', title='Distribution by Sample', ax=ax, c=colors[i])
+
+        # Adjust coordinates if no negative values
+        min = samp.values.min()
+        if min >= 0:
+            adjXCoord(ax)
+
         colLines.append(matplotlib.lines.Line2D([], [], c=colors[i], label=grp))
 
     return plt.legend(colLines, dat.levels, loc='upper left')
 
 
+def pltBySample(dat, ax):
+    """ Color Lines by Group
+
+    If no group information then color distribution lines by sample.
+
+    """
+    samp = dat.wide[dat.sampleIDs]
+    samp.plot(kind='kde', title='Distribution by Sample', ax=ax)
+
+    # Adjust coordinates if no negative values
+    min = samp.values.min()
+    if min >= 0:
+        adjXCoord(ax)
+
+
 def main(args):
     # Import data
     logger.info(u'Importing data with following parameters: \n\tWide: {0}\n\tDesign: {1}\n\tUnique ID: {2}\n\tGroup Column: {3}'.format(args.fname, args.dname, args.uniqID, args.group))
-    dat = wideToDesign(args.fname, args.dname, args.uniqID, args.group, clean_string=True)
+    dat = wideToDesign(args.fname, args.dname, args.uniqID, args.group)
     dat.wide.convert_objects(convert_numeric=True)
 
     fig = plt.figure(figsize=(20, 10))
@@ -72,16 +104,15 @@ def main(args):
         legend1 = pltByTrt(dat, ax)
     else:
         logger.info('Plotting sample distributions')
-        samp = dat.wide[dat.sampleIDs]
-        samp.plot(kind='kde', title='Distribution by Sample', ax=ax)
+        pltBySample(dat, ax)
 
+    # Create Legend
     handles, labels = ax.get_legend_handles_labels()
     plt.legend(handles, labels, ncol=5, loc='upper right', fontsize=10)
 
-    try:
+    # Create second legend if there is group information
+    if hasattr(dat, 'group'):
         ax.add_artist(legend1)
-    except:
-        pass
 
     plt.savefig(args.ofig, format='pdf')
     mpld3.save_html(fig, args.ofig2)
