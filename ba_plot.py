39fe82f09228815d21afa4869777d1ace5bd227b
Justin Fear <jfear@ufl.edu>
2015-04-27 10:59:10 -0400

    still playing with ba plots

--- ba_plot.py
+++ ba_plot.py
@@ -41,94 +41,45 @@ def getOptions():
     return(args)
 
 
-def runRegression(x, y):
-    """ Run a simple linear regression """
-    model = sm.OLS(y, x)
-    results = model.fit()
-    fitted = results.fittedvalues
-    prstd, lower, upper = wls_prediction_std(results)
-    return lower, upper, fitted
-
-
-def makeScatter(x, y, ax):
-    """ """
-
-    # Get Names of the combo
-    xname = x.name
-    yname = y.name
-
-    # Get Upper and Lower CI
-    upper, lower, fitted = runRegression(x, y)
-
-    # Plot
-    ax.plot(x, y, 'o')
-    ax.plot(x, fitted, 'k-')
-    ax.plot(x, upper, 'r--')
-    ax.plot(x, lower, 'r--')
-    ax.set_xlabel(xname)
-    ax.set_ylabel(yname)
-    ax.set_title('Scatter plot')
-
-    return ax
-
-
-def makeBA(v1, v2, ax):
-    """ Function to make BA Plot """
-
-    # Make BA plot
-    diff = v1 - v2
-    mean = (v1 + v2) / 2
-
-    # Get Upper and Lower CI
-    lower, upper, fitted = runRegression(mean, diff)
-    mask = (diff >= upper) | (diff <= lower)
-
-    # Plot
-    ax.scatter(x=mean[~mask], y=diff[~mask])
-    ax.scatter(x=mean[mask], y=diff[mask], color='r')
-
-    ax.axhline(0, color='k')
-    ax.plot(mean, upper, 'r--')
-    ax.plot(mean, lower, 'r--')
-
-    # Adjust axis
-    ax.set_xlabel('Mean')
-    ax.set_ylabel('Difference\n{0} - {1}'.format(v1.name, v2.name))
-    ax.set_title('BA Plot')
-    labels = ax.get_xmajorticklabels()
-    plt.setp(labels, rotation=45)
-
-    return ax, mask
-
-
-def buildTitle(xname, yname, group):
-    """ Build plot title """
-    if group:
-        title = '{0}\n{1} vs {2}'.format(group, xname, yname)
-    else:
-        title = '{0} vs {1}'.format(xname, yname)
-
-    return title
-
-
 class FlagOutlier:
-    """ """
+    """ Object for handling flags for outliers.
+
+    Here we are iterating of pairwise combinations of samples and creating BA
+    plots. If a sample falls outside of a 95% CI then want to flag these
+    samples and outliers. We will have a flag for each pairwise combination.
+
+    """
     def __init__(self, index):
-        """ """
+        """
+        Args:
+            index (pd.Index): Index containing a list of compounds, will be
+                used to create row index for flag table.
+
+        Attributes:
+            self.cnt (int): A counter for keep track of pairwise flag names.
+
+            self.flag_outlier (pd.DataFrame): Data frame containing compound as
+                row and flag name as columns.
+
+            self.design (pd.DataFrame): Data frame relating flag name back to
+                pairwise sample comparisons.
+
+        """
         self.cnt = 0
         self.flag_outlier = pd.DataFrame(index=index)
         self.design = pd.DataFrame(index=[0], columns=('cbn1', 'cbn2'))
 
-    def dropOutlier(self, data, group=False, acrossAll=False):
+    def dropOutlier(self, data, bygroup=False, acrossAll=False):
         """ """
         if acrossAll:
-            #TODO
+            # Drop row if a flag is present in any sample.
             pass
-        elif group:
-            #TODO
+        elif bygroup:
+            # Set values for entire group to NaN if flag is present in any
+            # sample within a group.
             pass
         else:
-            #TODO
+            # Set values to NaN if they were flagged.
             pass
 
         return
@@ -161,10 +112,35 @@ class FlagOutlier:
 
 
 def iterateCombo(data, combos, out, flags, group=None):
-    # Grab global counter
+    """ Iterate over pairwise combinations and generate plots.
 
+    This is a wrapper function to iterate over pairwise combinations and
+    generate plots.
+
+    Args:
+        data (pd.DataFrame): Data frame containing data in wide format compound
+            as rows and samples as columns.
+
+        combos (list): List of tuples of pairwise combinations of samples.
+
+        out (PdfPages): Handler for multi-page PDF that will contain all plots.
+
+        flags (FlagOutlier): FlagOutlier object.
+
+        group (str): Default is None if there are no groups. Otherwise it is
+            the name of the current group. This value will be used in plot
+            titles if present.
+
+    Returns:
+        out (PdfPages): A multi-page PDF containing scatter and BA plots.
+
+        flags (FlagOutlier): Updated FlagOutlier object containing a table of
+            outlier flags.
+
+    """
+    # Grab global counter
     for combo in combos:
-        # Set up figure
+        # Set up figure with 2 subplots
         fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5), dpi=300)
         fig.subplots_adjust(wspace=0.4)
 
@@ -187,8 +163,130 @@ def iterateCombo(data, combos, out, flags, group=None):
         flags.updateOutlier(combo[0], combo[1], outlier)
 
 
+def runRegression(x, y):
+    """ Run a linear regression.
+
+    Args:
+        x (pd.Series): Series of first sample, treated as independent variable.
+
+        y (pd.Series): Series of second sample, treated as dependent variables.
+
+    Returns:
+        lower (pd.Series): Series of values for lower confidence interval.
+
+        upper (pd.Series): Series of values for upper confidence interval.
+
+        fitted (pd.Series): Series of fitted values.
+
+    """
+    model = sm.OLS(y, x)
+    results = model.fit()
+    fitted = results.fittedvalues
+    prstd, lower, upper = wls_prediction_std(results)
+    return lower, upper, fitted
+
+
+def makeScatter(x, y, ax):
+    """ Plot a scatter plot of x vs y.
+
+    Args:
+        x (pd.Series): Series of first sample, treated as independent variable.
+
+        y (pd.Series): Series of second sample, treated as dependent variables.
+
+        ax (matplotlib axis): Axis which to plot.
+
+    Returns:
+        ax (mapltolib axis): Axis with a scatter plot comparing x vs y with a
+            regression line of fitted values (black) and the upper and lower
+            confidence intervals (red).
+
+    """
+    # Get Names of the combo
+    xname = x.name
+    yname = y.name
+
+    # Get Upper and Lower CI
+    lower, upper, fitted = runRegression(x, y)
+
+    # Plot
+    ax.plot(x, y, 'o')
+    ax.plot(x, lower, 'r--')
+    ax.plot(x, fitted, 'k-')
+    ax.plot(x, upper, 'r--')
+    ax.set_xlabel(xname)
+    ax.set_ylabel(yname)
+    ax.set_title('Scatter plot')
+
+    return ax
+
+
+def makeBA(x, y, ax):
+    """ Function to make BA Plot comparing x vs y.
+
+    Args:
+        x (pd.Series): Series of first sample, treated as independent variable.
+
+        y (pd.Series): Series of second sample, treated as dependent variables.
+
+        ax (matplotlib axis): Axis which to plot.
+
+    Returns:
+        ax (mapltolib axis): Axis with a Bland-Altman plot comparing x vs y with a
+            horizontal line at y = 0 (black) and the upper and lower
+            confidence intervals (red).
+
+        mask (pd.Series BOOL): Series containing Boolean values with True
+            indicating a value is more extreme than CI and should be an outlier and
+            False indicating a value falls inside CI.
+
+    """
+    # Make BA plot
+    diff = x - y
+    mean = (x + y) / 2
+
+    # Get Upper and Lower CI
+    lower, upper, fitted = runRegression(mean, diff)
+    mask = (diff >= upper) | (diff <= lower)
+
+    # Plot
+    ax.scatter(x=mean[~mask], y=diff[~mask])
+    ax.scatter(x=mean[mask], y=diff[mask], color='r')
+
+    ax.plot(mean, lower, 'r--')
+    ax.axhline(0, color='k')
+    ax.plot(mean, upper, 'r--')
+
+    # Adjust axis
+    ax.set_xlabel('Mean')
+    ax.set_ylabel('Difference\n{0} - {1}'.format(x.name, y.name))
+    ax.set_title('BA Plot')
+    labels = ax.get_xmajorticklabels()
+    plt.setp(labels, rotation=45)
+
+    return ax, mask
+
+
+def buildTitle(xname, yname, group):
+    """ Build plot title.
+    Args:
+        xname (str): String containing the sampleID for x
+
+        yname (str): String containing the sampleID for y
+
+        gropu (str): String containing the group information. If no group then
+            None.
+
+    """
+    if group:
+        title = '{0}\n{1} vs {2}'.format(group, xname, yname)
+    else:
+        title = '{0} vs {1}'.format(xname, yname)
+
+    return title
+
+
 def main(args):
-    """ """
     # Import data
     dat = wideToDesign(args.fname, args.dname, args.uniqID, args.group)
     wide = dat.wide[dat.sampleIDs]
