6898aec27ebac0fd33ecc14e90d5bd8f28086397
Justin Fear <jfear@ufl.edu>
2015-05-06 13:24:39 -0400

    Made sure indices are strings even if they are numeric.

--- interface.py
+++ interface.py
@@ -71,6 +71,11 @@ class wideToDesign:
             self.wide = pd.read_table(wide)
             if clean_string:
                 self.wide[self.uniqID] = self.wide[self.uniqID].apply(lambda x: self._cleanStr(x))
+
+            # Make sure index is a string and not numeric
+            self.wide[self.uniqID] = self.wide[self.uniqID].astype(str)
+
+            # Set index to uniqID column
             self.wide.set_index(self.uniqID, inplace=True)
         except:
             print "Please make sure that your data file has a column called '{0}'.".format(uniqID)
@@ -78,15 +83,19 @@ class wideToDesign:
 
         # Import design file
         try:
-            self.design = pd.read_table(design, index_col='sampleID')
+            self.design = pd.read_table(design)
+
+            # Make sure index is a string and not numeric
+            self.design['sampleID'] = self.design['sampleID'].astype(str)
+            self.design.set_index('sampleID', inplace=True)
 
             # Create a list of sampleIDs, but first check that they are present
             # in the wide data.
             self.sampleIDs = list()
 
             for sample in self.design.index.tolist():
-                if str(sample) in self.wide.columns:
-                    self.sampleIDs.append(str(sample))
+                if sample in self.wide.columns:
+                    self.sampleIDs.append(sample)
         except:
             print "Please make sure that your design file has a column called 'sampleID'."
             raise ValueError
