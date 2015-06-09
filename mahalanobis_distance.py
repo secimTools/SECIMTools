63995b4524f8a6c20f5d12407807da205e63e0f9
Jonathan <poissonj@ufl.edu>
2015-06-09 17:07:12 -0400

    Added console commands...kinda

--- mahalanobis_distance.py
+++ mahalanobis_distance.py
@@ -8,24 +8,22 @@ import logging
 # Add-on packages
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # McLab packages
-import mclib
+# import mclib
 
 
 def getOptions():
     """ Function to pull in arguments """
-    #TODO: Add cmd line options, I just copied and pasted these.
     description = """ """
     parser = argparse.ArgumentParser(description=description, formatter_class=RawDescriptionHelpFormatter)
 
     group1 = parser.add_argument_group(description="Input Files")
-    group1.add_argument("--vcf", dest="vcfName", action='store', required=True, help="Name of VCF file. If not zipped using bgzip will try to create zip. [Required]")
+    group1.add_argument("--input", dest="wide", action='store', required=True, help="Dataset in Wide format")
 
     group2 = parser.add_argument_group(description="Output Files")
-    group2.add_argument("-o", dest="oname", action='store', required=True, help="Name of output FASTA. [Required]")
-    group2.add_argument("--log", dest="log", action='store', required=False, help="Name of the LOG file [Optional]")
-    parser.add_argument("--debug", dest="debug", action='store_true', required=False, help="Enable debug output.")
+    group2.add_argument("-o", dest="plot", action='store', required=True, help="Output of mahalanobis distanace plot")
 
     args = parser.parse_args()
     return(args)
@@ -36,16 +34,16 @@ def mahalnobisDistance(wide):
 
     Arguments:
         :type wide: pandas.DataFrame
-        :param wide: A wide formated data frame with samples as columns and compounds as rows.
+        :param wide: A wide formatted data frame with samples as columns and compounds as rows.
 
     Returns:
         :return: Return a numpy array with MD values.
         :rtype: numpy.array
     """
-    # Covaranice matrix from the data
+    # Covariance matrix from the data
     covHat = wide.cov()
 
-    # Inverse of the covarance matrix
+    # Inverse of the covariance matrix
     covHatInv = np.linalg.inv(covHat)
 
     # Column means
@@ -57,7 +55,8 @@ def mahalnobisDistance(wide):
     # Calculate the mahalanobis distance sqrt{(Yi - Ybar)^T Sigma^-1 (Yi - Ybar)}
     MD = np.diag(np.sqrt(np.dot(np.dot(center.T, covHatInv), center)))
 
-    return MD
+    # TODO: Maybe output the MD file in the future?
+    return plotMahalanobis(MD)
 
 
 def plotMahalanobis(MD):
@@ -80,14 +79,16 @@ def plotMahalanobis(MD):
     # Add a horizontal line above 95% of the data
     ax.axhline(np.percentile(MD, 95), ls='--', lw=2)
 
-    #TODO: Instead of returning the figure we could go ahead and write it out to a file.
-
     return fig
 
 
 def main(args):
     """ Main Script """
-    #TODO: Add the main arguments
+    # Convert inputted wide file to dataframe
+    wideDataFrame = pd.DataFrame.from_csv(args.wide)
+    figure = mahalnobisDistance(wideDataFrame)
+
+    figure.savefig(args.plot)
 
 if __name__ == '__main__':
     # Turn on Logging if option -g was given
@@ -95,13 +96,13 @@ if __name__ == '__main__':
 
     # Turn on logging
     logger = logging.getLogger()
-    if args.debug:
-        mclib.logger.setLogger(logger, args.log, 'debug')
-    else:
-        mclib.logger.setLogger(logger, args.log)
+    # if args.debug:
+    #     mclib.logger.setLogger(logger, args.log, 'debug')
+    # else:
+    #     mclib.logger.setLogger(logger, args.log)
 
     # Output git commit version to log, if user has access
-    mclib.git.git_to_log(__file__)
+    # mclib.git.git_to_log(__file__)
 
     # Run Main part of the script
     main(args)
