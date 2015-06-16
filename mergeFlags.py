0adbbd852e1170681b450a07e913df13316a3626
Jonathan <poissonj@ufl.edu>
2015-06-16 11:15:42 -0400

    Added comments to mergeFlags.py

--- mergeFlags.py
+++ mergeFlags.py
@@ -39,6 +39,7 @@ def mergeFlags(args):
     if ',' in args.flagFiles[0]:
         args.flagFiles = args.flagFiles[0].split(',')
 
+    # Convert files into dataframes and populate into new list
     for flagFile in args.flagFiles:
         dataFrame = pd.DataFrame.from_csv(flagFile, sep='\t')
         flagDataFrameList.append(dataFrame)
@@ -47,6 +48,7 @@ def mergeFlags(args):
     counter = 0
     try:
         while counter < len(flagDataFrameList) - 1:
+            # Check the index of a dataframe compared to the rest of the dataframe's indexes
             if flagDataFrameList[counter].index.equals(flagDataFrameList[counter + 1].index):
                 counter += 1
             else:
@@ -55,9 +57,13 @@ def mergeFlags(args):
         logger.error("Not all indexes are the same")
         raise SystemExit
 
+    # Merge flags together
     mergedFlags = pd.concat(flagDataFrameList, axis=1)
+    # Export merged flags
     mergedFlags.to_csv(args.mergedFile, sep='\t')
+
 def main(args):
+    # Call mergeFlags function. Main is used for convention
     mergeFlags(args)
 
 
