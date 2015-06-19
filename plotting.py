30668aa38b52556510d5cf0512291a7077b49af7
Justin Fear <jfear@ufl.edu>
2015-06-19 13:57:01 -0400

    Adds plotting module

---
+++ plotting.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+""" Set of helper function and variables for plotting.
+
+This module provides a set of functions and variables that will be useful for
+plotting.
+
+"""
+
+
+class ColorMarker:
+    def __init__(self):
+        # A list of colors
+        self._colors = ['k', 'b', 'g', 'c', 'm', 'y']
+        # A list of markers
+        self._markers = ['o', 's', '^', 'D', 'd', 'h', 'x', '*', '+', 'v', '<', '>', '1', '2', '3', '4', '8', 'p', 'H']
+
+    def get_colors(self):
+        """ Get a set of color/marker combinations.
+
+        :rtype: list of tuple
+        :returns: A list of tuples containing color|marker pairs. There are a total
+            of 114 combinations. Red and white are not used in this color scheme.
+            Red is reserved for coloring points beyond a threshold, and white does not
+            show up on white backgrounds.
+
+        """
+        comb = list()
+        for i in self._markers:
+            for j in self._colors:
+                comb.append((j, i))
+
+        return comb
