b494afebaca1e158be887dc27c827c082412582e
Jonathan <poissonj@ufl.edu>
2015-07-02 19:30:18 -0400

    Clean up CVflag.py and implement Flags interface

--- CVflag.py
+++ CVflag.py
@@ -19,7 +19,7 @@ import pandas as pd
 
 # Local packages
 from interface import wideToDesign
-import logger as sl
+from interface import Flags
 
 # Glocals
 global DEBUG
@@ -29,100 +29,90 @@ def getOptions(myopts=None):
     """Function to pull in arguments"""
     description = """Retention Time flags are ... """
     parser = argparse.ArgumentParser(description = description, formatter_class = RawDescriptionHelpFormatter)
-    parser.add_argument("--input", dest = "fname", action = 'store', required = True, help = "Input dataset in wide format.")
-    parser.add_argument("--design", dest = "dname", action = 'store', required = True, help = "Design file.")
-    parser.add_argument("--ID", dest = "uniqID", action = 'store', required = True, help = "Name of the column with unique identifiers.")
-    #parser.add_argument("--log", dest = "log", action = 'store_true', required = False, default = False, help = "Put this argument if the data needs rounding to 3 significant digits and a log transformation (Usually for peak area and peak height). [option]")
-    parser.add_argument("--debug", dest = "debug", action = 'store_true', required = False, help = "Add debugging log output. [optional]")
-    parser.add_argument("--group", dest = "group", action = 'store', required = False, default = False, help = "Add option to separate sample IDs by treatment name. [optional]")
-    parser.add_argument("--outPath", dest = "outPath", action = 'store', required = True, help = "Path to save created files.")
-    #parser.add_argument("--CVflag", dest = "outfile", action = 'store', required = False, default = 'CVflag', help = "Output filename of CV flags. The default is [CVflag]. [optional]")
-    parser.add_argument("--CVcutoff", dest = "CVcutoff", action = 'store', required = False, default = False, help = "The default CV cutoff will flag 10 percent of the rowIDs with larger CVs. If you want to set a CV cutoff, put the number here. [optional]")
-    #parser.add_argument("--CVplot", dest = "CVplot", action = 'store', required = False, default = 'CVplot', help = "Output filename of CV plot. The default is [CVplot]. [optional]")
 
-    if myopts:
-        args = parser.parse_args(myopts)
-    else:
-        args = parser.parse_args()
+    group1 = parser.add_argument_group(title='Standard input', description='Standard input for SECIM tools.')
+    group1.add_argument("--input", dest = "fname", action = 'store', required = True, help = "Input dataset in wide format.")
+    group1.add_argument("--design", dest = "dname", action = 'store', required = True, help = "Design file.")
+    group1.add_argument("--ID", dest = "uniqID", action = 'store', required = True, help = "Name of the column with unique identifiers.")
+    group1.add_argument("--outPath", dest = "outPath", action = 'store', required = True, help = "Path to save created files. Example: ~/Desktop/output/  The output folder must have been created already.")
+
+    group2 = parser.add_argument_group(title='Standard input', description='Standard input for SECIM tools.')
+    group2.add_argument("--group", dest = "group", action = 'store', required = False, default = False, help = "Add option to separate sample IDs by treatment name. [optional]")
+    group2.add_argument("--CVcutoff", dest = "CVcutoff", action = 'store', required = False, default = False, help = "The default CV cutoff will flag 10 percent of the rowIDs with larger CVs. If you want to set a CV cutoff, put the number here. [optional]")
+
+    args = parser.parse_args()
 
     return(args)
 
-def setCVflagByGroup(args, wide, dat, dir):
+def setCVflagByGroup(args, wide, dat):
 
     # Split design file by treatment group
-    
-    try:
-        pdfOut = PdfPages(dir + '/CVplot_by_' + args.group + '.pdf') # (dir + '/' + args.CVplot + '_by_' + args.group + '.pdf')
-        CV = pd.DataFrame(index=wide.index)
-        for title, group in dat.design.groupby(args.group):
 
-            # Filter the wide file into a new dataframe
-            currentFrame = wide[group.index]
+    pdfOut = PdfPages(args.outPath + '/CVplot_by_' + args.group + '.pdf')
+    CV = pd.DataFrame(index=wide.index)
+    for title, group in dat.design.groupby(args.group):
 
-            # Change dat.sampleIDs to match the design file
-            dat.sampleIDs = group.index
+        # Filter the wide file into a new dataframe
+        currentFrame = wide[group.index]
 
-            CV['cv_'+title], CVcutoff = setCVflag(args, currentFrame, dat, dir=dir, groupName=title)
-            
-        CV['cv'] = CV.apply(np.max, axis=1)
-        if not args.CVcutoff:
-            CVcutoff = np.percentile(CV['cv'].values, q=90)
-            CVcutoff = round(CVcutoff, -int(floor(log(CVcutoff, 10))) + 2)
-        else:
-            CVcutoff = args.CVcutoff
-        for title, group in dat.design.groupby(args.group):        
-            fig, ax = plt.subplots()
-            xmin = -np.percentile(CV['cv_'+title].values,99)*0.2
-            xmax = np.percentile(CV['cv_'+title].values,99)*1.5
-            ax.set_xlim(xmin, xmax)
-            CV['cv_'+title].plot(kind='hist', range = (xmin, xmax), bins = 15, normed = 1, color = 'grey', label = "CV histogram")
-            CV['cv_'+title].plot(kind='kde', title="Density Plot of Coefficients of Variation in " + args.group + " " + title, ax=ax, label = "CV density")
-            plt.axvline(x=CVcutoff, color = 'red', linestyle = 'dashed', label = "Cutoff at: {0}".format(CVcutoff))
-            plt.legend()
-            pdfOut.savefig(fig, bbox_inches='tight')
-            plt.close(fig)
-        
+        # Change dat.sampleIDs to match the design file
+        dat.sampleIDs = group.index
+
+        CV['cv_'+title], CVcutoff = setCVflag(args, currentFrame, dat, groupName=title)
+
+    CV['cv'] = CV.apply(np.max, axis=1)
+    if not args.CVcutoff:
+        CVcutoff = np.percentile(CV['cv'].values, q=90)
+        CVcutoff = round(CVcutoff, -int(floor(log(CVcutoff, 10))) + 2)
+    else:
+        CVcutoff = args.CVcutoff
+    for title, group in dat.design.groupby(args.group):
         fig, ax = plt.subplots()
-        xmin = -np.percentile(CV['cv'].values,99)*0.2
-        xmax = np.percentile(CV['cv'].values,99)*1.5
+        xmin = -np.percentile(CV['cv_'+title].values,99)*0.2
+        xmax = np.percentile(CV['cv_'+title].values,99)*1.5
         ax.set_xlim(xmin, xmax)
-        for title, group in dat.design.groupby(args.group):
-            CV['cv_'+title].plot(kind='kde', title="Density Plot of Coefficients of Variation by " + args.group, ax=ax, label = "CV density in group "+title)
+        CV['cv_'+title].plot(kind='hist', range = (xmin, xmax), bins = 15, normed = 1, color = 'grey', label = "CV histogram")
+        CV['cv_'+title].plot(kind='kde', title="Density Plot of Coefficients of Variation in " + args.group + " " + title, ax=ax, label = "CV density")
         plt.axvline(x=CVcutoff, color = 'red', linestyle = 'dashed', label = "Cutoff at: {0}".format(CVcutoff))
         plt.legend()
         pdfOut.savefig(fig, bbox_inches='tight')
-        plt.close(fig)    
-        pdfOut.close()
-        return CV['cv'], CVcutoff
+        plt.close(fig)
 
-    except KeyError as e:
-        logger.error("{} is not a column name in the design file.".format(args.group))
-    except Exception as e:
-        logger.error("Error. {}".format(e))
+    fig, ax = plt.subplots()
+    xmin = -np.percentile(CV['cv'].values,99)*0.2
+    xmax = np.percentile(CV['cv'].values,99)*1.5
+    ax.set_xlim(xmin, xmax)
+    for title, group in dat.design.groupby(args.group):
+        CV['cv_'+title].plot(kind='kde', title="Density Plot of Coefficients of Variation by " + args.group, ax=ax, label = "CV density in group "+title)
+    plt.axvline(x=CVcutoff, color = 'red', linestyle = 'dashed', label = "Cutoff at: {0}".format(CVcutoff))
+    plt.legend()
+    pdfOut.savefig(fig, bbox_inches='tight')
+    plt.close(fig)
+    pdfOut.close()
+    return CV['cv'], CVcutoff
 
-def setCVflag(args, wide, dat, dir, groupName = ''):
-    
+
+def setCVflag(args, wide, dat, groupName = ''):
+
     # Round all values to 3 significant digits
     DATround = wide.applymap(lambda x: x)
-    #if args.log:
-    #    DATround = DATround.applymap(lambda x: log(round(x, -int(floor(log(abs(x), 10))) + 2)))
-    
-    # Get std, mean and calculate CV
+
+    # Get std, mean and calculate CV #
     DATstat = pd.DataFrame(index=DATround.index)
+
     DATstat['std']  = DATround.apply(np.std, axis=1)
     DATstat['mean'] = DATround.apply(np.mean, axis=1)
     DATstat['cv']   = DATstat['std'] / DATstat['mean']
-    
+
     if not args.CVcutoff:
         CVcutoff = np.percentile(DATstat['cv'].values, q=90)
         CVcutoff = round(CVcutoff, -int(floor(log(CVcutoff, 10))) + 2)
     else:
         CVcutoff = args.CVcutoff
-    
+
     # Plot CVs
     if groupName == '':
         fig, ax = plt.subplots()
-        #xmin, xmax = ax.get_xlim()
         xmin = -np.percentile(DATstat['cv'].values,99)*0.2
         xmax = np.percentile(DATstat['cv'].values,99)*1.5
         ax.set_xlim(xmin, xmax)
@@ -130,22 +120,16 @@ def setCVflag(args, wide, dat, dir, groupName = ''):
         DATstat['cv'].plot(kind='kde', title="Density Plot of Coefficients of Variation", ax=ax, label = "CV density")
         plt.axvline(x=CVcutoff, color = 'red', linestyle = 'dashed', label = "Cutoff at: {0}".format(CVcutoff))
         plt.legend()
-        CVplotFileName = 'CVplot.pdf' # args.CVplot + '.pdf'
-        CVplotFileNameWithDir = dir + '/' + CVplotFileName
-        plt.savefig(CVplotFileNameWithDir, format='pdf')
+
+        # Set file name of pdf and export
+        CVplotFileName = args.outPath + '/CVplot.pdf'
+        plt.savefig(CVplotFileName, format='pdf')
         plt.close(fig)
-    
+
     return DATstat['cv'], CVcutoff
 
 def main(args):
-    """ """
-    directory = args.outPath
-    # create a directory in galaxy to hold the files created
-    try: # Copied from countDigits.py
-        os.makedirs(args.outPath)
-    except Exception, e:
-        logger.error("Error. {}".format(e))
-    
+
     # Import data
     dat = wideToDesign(args.fname, args.dname, args.uniqID)
 
@@ -154,46 +138,25 @@ def main(args):
 
     # Use group separation or not depending on user input
     if not args.group:
-        DATstat, CVcutoff = setCVflag(args, wide, dat, dir = directory)
-        CVflagFileName = 'CVflag' # args.outfile
+        DATstat, CVcutoff = setCVflag(args, wide, dat)
+        CVflagFileName = 'CVflag'
     else:
-        DATstat, CVcutoff = setCVflagByGroup(args, wide, dat, dir = directory)
-        CVflagFileName = 'CVflag_by_' + args.group # args.outfile + '_by_' + args.group
-    CVflag = pd.DataFrame(index=DATstat.index)
-    CVflag['flag_big_CV'] = DATstat > CVcutoff
-    CVflag = CVflag.applymap(lambda x: int(x))
-    CVflagFileNameDir = directory + '/' + CVflagFileName + '.tsv'
-    CVflagFile = open(CVflagFileNameDir, 'w')
-    CVflag.to_csv(CVflagFileNameDir, sep='\t')
-    CVflagFile.close()    
+        DATstat, CVcutoff = setCVflagByGroup(args, wide, dat)
+        CVflagFileName = 'CVflag_by_' + args.group
+
+    #Create CVflag DataFrame
+    CVflag = Flags(index=DATstat.index)
+
+    # Fill in DataFrame values
+    CVflag.addColumn(column = 'flag_big_CV', mask = DATstat > CVcutoff)
+
+    #Set the file name and export
+    CVflag.df_flags.to_csv(args.outPath + CVflagFileName + '.tsv', sep='\t')
 
 if __name__ == '__main__':
 
     # Command line options
     args = getOptions()
-    
-    logger = logging.getLogger()
-    if args.debug:
-        sl.setLogger(logger, logLevel='debug')
-    else:
-        sl.setLogger(logger)
 
     main(args)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
