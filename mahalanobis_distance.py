bc787e7b6234aee64ddfb3f767c422eff0d582fc
Jonathan <poissonj@ufl.edu>
2015-07-14 13:47:13 -0400

    Mahalanobis docstring fixes

--- mahalanobis_distance.py
+++ mahalanobis_distance.py
@@ -13,8 +13,14 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.stats as stats
+from matplotlib.backends.backend_pdf import PdfPages
+
+# Local packages
+from interface import wideToDesign
+import plotting
 
 
+iteratorOfColors = 0
 def getOptions():
     """ Function to pull in arguments """
     description = """ """
@@ -22,9 +28,13 @@ def getOptions():
 
     group1 = parser.add_argument_group(description="Input Files")
     group1.add_argument("--input", dest="wide", action='store', required=True, help="Dataset in Wide format")
+    group1.add_argument("--design", dest="design", action='store', required=True, help="Dataset in Design format")
+    parser.add_argument("--ID", dest="uniqID", action='store', required=True, help="Name of the column with unique identifiers.")
+    group1.add_argument("--group", dest="group", action='store', required=False, default='', help="Separate plots by group. Must specify valid group name")
 
     group2 = parser.add_argument_group(description="Output Files")
-    group2.add_argument("-o", dest="plot", action='store', required=True, help="Output of Mahalanobis distance plot")
+    group2.add_argument("-o", dest="outPDF", action='store', required=True, help="Output of Mahalanobis distance PDF of plots")
+    group2.add_argument("--flags", dest="flags", action='store', required=True, help="Output flag file")
 
     args = parser.parse_args()
     return(args)
@@ -33,14 +43,16 @@ def getOptions():
 def mahalnobisDistance(wide):
     """ Calculate the Mahalanobis distance and return an array of distances.
 
-    Arguments:
-        :type wide: pandas.DataFrame
-        :param wide: A wide formatted data frame with samples as columns and compounds as rows.
+            Arguments:
+                :type wide: pandas.DataFrame
+                :param wide: A wide formatted data frame with samples as columns and compounds as rows.
 
-    Returns:
-        :return: Return a numpy array with MD values.
-        :rtype: numpy.array
-    """
+                :param type string: The name of the title of the plot if using group separation
+
+            Returns:
+                :return: Return a numpy array with MD values.
+                :rtype: numpy.array
+            """
     # Covariance matrix from the data
     covHat = wide.cov()
 
@@ -56,46 +68,43 @@ def mahalnobisDistance(wide):
     # Calculate the mahalanobis distance sqrt{(Yi - Ybar)^T Sigma^-1 (Yi - Ybar)}
     MD = np.diag(np.sqrt(np.dot(np.dot(center.T, covHatInv), center)))
 
-    # Calculate cutoffs
-    numParam = wide.shape[1]
-    c99 = np.sqrt(stats.chi2.ppf(0.995, numParam))
-    c97 = np.sqrt(stats.chi2.ppf(0.975, numParam))
-    c95 = np.sqrt(stats.chi2.ppf(0.95, numParam))
-    cutoffs = (('99.5%', c99), ('97.5%', c97), ('95.0%', c95))
+    # # Calculate cutoffs
+    # numParam = wide.shape[1]
+    # c99 = np.sqrt(stats.chi2.ppf(0.995, numParam))
+    # c97 = np.sqrt(stats.chi2.ppf(0.975, numParam))
+    # c95 = np.sqrt(stats.chi2.ppf(0.95, numParam))
+    # cutoffs = (('99.5%', c99), ('97.5%', c97), ('95.0%', c95))
 
     # TODO: Create a flag based on values greater than one of the cutoffs.
-    return plotMahalanobis(MD, cutoffs)
+    # return plotMahalanobis(MD, cutoffs, title)
+    return MD
 
-
-def plotMahalanobis(MD, cutoffs):
+def plotMahalanobis(MD, color):
     """ Plot the Mahalanobis distance plot.
 
-    Arguments:
-        :type MD: numpy.array
-        :param MD: An array of distances.
+                Arguments:
+                    :type MD: numpy.array
+                    :param MD: An array of distances.
 
-        :param tuple cutoffs: A tuple with label and cutoff. Used a tuple so that sort order would be maintained.
+                    :param tuple cutoffs: A tuple with label and cutoff. Used a tuple so that sort order would be maintained.
 
-    Returns:
-        :return: Returns a figure object.
-        :rtype: matplotlib.pyplot.Figure.figure
+                    :param type string: The name of the title of the plot if using group separation
 
-    """
-    # Create figure object with a single axis
-    fig, ax = plt.subplots(1, 1, figsize=(8, 8))
-    fig.suptitle('Mahalanobis Distance')
+                Returns:
+                    :return: Returns a figure object.
+                    :rtype: matplotlib.pyplot.Figure.figure
+
+                """
+
+
 
     # Plot MD as scatter plot
-    ax.scatter(x=range(len(MD)), y=MD)
+    ax.scatter(x=range(MD.size), y=MD, c=color)
     ax.set_xlabel('Mean')
     ax.set_ylabel('Difference')
+    ax.legend()
 
-    # Add a horizontal line above 95% of the data
-    colors = ['r', 'y', 'c', 'b', 'k', 'm', 'g']
-    for i, val in enumerate(cutoffs):
-        ax.axhline(val[1], color=colors[i], ls='--', lw=2, label='{} $\chi^2$'.format(val[0]))
-    plt.legend()
-    return fig
+
 
 
 def galaxySavefig(fig, fname):
@@ -111,22 +120,86 @@ def galaxySavefig(fig, fname):
     os.remove(png_out)
 
 
-def main(args):
+def main():
     """ Main Script """
 
     # Convert inputted wide file to dataframe
-    df_trans = pd.DataFrame.from_csv(args.wide, sep='\t').T
-    figure = mahalnobisDistance(df_trans)
+    # df_trans = pd.DataFrame.from_csv(args.wide, sep='\t').T
+    dat = wideToDesign(args.wide, args.design, args.uniqID)
+    df_trans = dat.wide.transpose()
 
-    galaxySavefig(fig=figure, fname=args.plot)
+    # Calculate cutoffs
+    numParam = dat.wide.shape[1]
+    c99 = np.sqrt(stats.chi2.ppf(0.995, numParam))
+    c97 = np.sqrt(stats.chi2.ppf(0.975, numParam))
+    c95 = np.sqrt(stats.chi2.ppf(0.95, numParam))
+    cutoffs = (('99.5%', c99), ('97.5%', c97), ('95.0%', c95))
+
+    #Import colors and markers
+    colorsAndMarkers = plotting.ColorMarker()
+    colorsAndMarkers = colorsAndMarkers.get_colors()
+
+    listOfMD = []
+
+
+    # Open a multiple page PDF for plots
+    outPDF = PdfPages(args.outPDF)
+    # If using groups:
+    if not args.group is '':
+        try:
+            for title, group in dat.design.groupby(args.group):
+
+                # Filter the transposed wide file into a new dataframe
+                currentFrame = df_trans.reindex(index=group.index)
+
+                # Save figure of the group
+                MD = mahalnobisDistance(currentFrame)
+                # galaxySavefig(fig=figure, fname=args.plot + '_' + title)
+                listOfMD.append(MD)
+        except KeyError:
+            logger.error("{} is not a column name in the design file.".format(args.group))
+        except Exception as e:
+            logger.error("Error. {}".format(e))
+
+    else:
+        # If not using groups:
+        figure = mahalnobisDistance(df_trans)
+        outPDF.savefig(figure)
+
+
+
+    global fig
+    global ax
+    global legendList
+    legendList=[]
+    # Create figure object with a single axis
+    fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+    fig.suptitle('Mahalanobis Distance')
+    # Add a horizontal line above 95% of the data
+    colors = ['r', 'y', 'c', 'b', 'k', 'm', 'g']
+    for i, val in enumerate(cutoffs):
+        ax.axhline(val[1], color=colors[i], ls='--', lw=2, label='{} $\chi^2$'.format(val[0]))
+    plt.legend()
+
+    for color, md in zip(colors,listOfMD):
+        plotMahalanobis(md,color)
+
+    outPDF.savefig(fig)
+
+    outPDF.close()
+    # galaxySavefig(fig=figure, fname=args.plot)
     # figure.savefig(args.plot + '.png', bbox_inches='tight')
+
 
 
 if __name__ == '__main__':
     # Turn on Logging if option -g was given
+
+    global args
     args = getOptions()
 
     # Turn on logging
+    logging.basicConfig()
     logger = logging.getLogger()
     # if args.debug:
     #     mclib.logger.setLogger(logger, args.log, 'debug')
@@ -137,5 +210,5 @@ if __name__ == '__main__':
     # mclib.git.git_to_log(__file__)
 
     # Run Main part of the script
-    main(args)
+    main()
     logger.info("Script complete.")
