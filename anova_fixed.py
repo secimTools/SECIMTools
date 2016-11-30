604e6e180bd5c10227b1e5bc79a9fa2f1653aa8e
miguelib <ibarrarellano@gmail.com>
2016-11-30 10:55:08 -0500

    Dealing with missing data in ANOVA

--- anova_fixed.py
+++ anova_fixed.py
@@ -69,9 +69,45 @@ def getOptions():
 
     return(args)
 
+def dropMissing(wide):
+    """
+    Drops missing data out of the wide file
+
+    :Arguments:
+        :type wide: pandas.core.frame.DataFrame
+        :param wide: DataFrame with the wide file data
+
+    :Returns:
+        :rtype wide: pandas.core.frame.DataFrame
+        :return wide: DataFrame with the wide file data without missing data
+    """
+    #Warning
+    logger.warn("Missing values were found")
+
+    #Count of original
+    nRows = len(wide.index)      
+
+    #Dropping 
+    wide.dropna(inplace=True)    
+
+    #Count of dropped
+    nRowsNoMiss = len(wide.index)  
+
+    #Warning
+    logger.warn("{} rows were dropped because of missing values.".
+                format(nRows - nRowsNoMiss))
+    return wide
+
 def main(args):
     # Import data
     dat = wideToDesign(args.input,args.design,args.uniqID)
+
+    # Treat everything as numeric
+    dat.wide = dat.wide.applymap(float)
+
+    #Dropping missing values
+    if np.isnan(dat.wide.values).any():
+        dat.wide = dropMissing(dat.wide)
 
     # Generate formula Formula
     preFormula,categorical,numerical,levels,dat.design = preProcessing(design=dat.design,
