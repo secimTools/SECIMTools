8558de0b00f15b15e2337ddd223a802d7debdbcb
miguelib <ibarrarellano@gmail.com>
2016-12-14 09:21:48 -0500

    multiple testing adjustment update, now outputs flag file

--- multiple_testing_adjustment.py
+++ multiple_testing_adjustment.py
@@ -25,11 +25,12 @@ import statsmodels.sandbox.stats.multicomp as stm
 # Local Packages
 import logger as sl
 from interface import wideToDesign
+from flags import Flags
 
 def getOptions():
     """Function to pull arguments"""
-    parser = argparse.ArgumentParser(description="Takes a peak area/heigh \
-                                     dataset and calculates the LOD on it ")
+    parser = argparse.ArgumentParser(description="Takes a peak area/heigh" \
+                                    " dataset and calculates the LOD on it ")
 
     #Standar input for SECIMtools
     standar = parser.add_argument_group(title='Standard input', description= 
@@ -42,12 +43,14 @@ def getOptions():
     standar.add_argument("-pv","--pval",dest="pval",action="store",
                         required=True, help="Name of the column with p-value.")
     standar.add_argument("-a","--alpha",dest="alpha",action="store",type=float,
-                        required=False,default=0.5, help="Alpha value.")
+                        required=False,default=0.05, help="Alpha value.")
 
     #Output Paths
     output = parser.add_argument_group(title='Output paths', description=
                                        "Paths for the output files")
     output.add_argument("-on","--outadjusted",dest="outadjusted",action="store",
+                        required=True,help="Output path for corrected file[TSV]")
+    output.add_argument("-fl","--flags",dest="flags",action="store",
                         required=True,help="Output path for flags file[TSV]")
 
     args = parser.parse_args()
@@ -66,14 +69,22 @@ def main(args):
     # Making bonferroni, Benjamini/Hochberg, Benjamini/Yekutieli
     logger.info("Runnig corrections")
     bonferroni = pd.Series(stm.multipletests(justPvals,alpha=args.alpha,
-                    returnsorted=False, method="bonferroni")[1],name=args.pval+"_bonferroni",
-                    index=toCorrect_df.index)
+                    returnsorted=False, method="bonferroni")[1],
+                    name=args.pval+"_bonferroni", index=toCorrect_df.index)
     bHochberg  = pd.Series(stm.multipletests(justPvals,alpha=args.alpha,
-                    returnsorted=False, method="fdr_bh")[1],name=args.pval+"_bHochberg",
-                    index=toCorrect_df.index)
+                    returnsorted=False, method="fdr_bh")[1],
+                    name=args.pval+"_bHochberg", index=toCorrect_df.index)
     bYekutieli = pd.Series(stm.multipletests(justPvals,alpha=args.alpha,
-                    returnsorted=False, method="fdr_by")[1],name=args.pval+"_bYekutieli",
-                    index=toCorrect_df.index)
+                    returnsorted=False, method="fdr_by")[1],
+                    name=args.pval+"_bYekutieli", index=toCorrect_df.index)
+
+    #Creating objet with flags
+    significance_flags = Flags(index=toCorrect_df.index)
+
+    #Add a column for each correction
+    for test in [bonferroni,bHochberg,bYekutieli]:
+        significance_flags.addColumn(column="flag_{0}_off".format(test.name),
+            mask=(test>args.alpha))
     
     # Concatenating results with pvals
     results = pd.concat([toCorrect_df[args.pval],bonferroni,bHochberg,bYekutieli],axis=1)
@@ -81,6 +92,9 @@ def main(args):
     # Saving data
     logger.info("Saving results")
     results.to_csv(os.path.abspath(args.outadjusted),sep="\t")
+
+    logger.info("Saving flags")
+    significance_flags.df_flags.to_csv(os.path.abspath(args.flags),sep="\t")
 
 if __name__ == '__main__':
     # Import data
