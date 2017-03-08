6228bd3985082aecf10cc27b97ee71151e850b3e
miguelib <ibarrarellano@gmail.com>
2017-03-08 14:21:36 -0500

    standardize paths

--- multiple_testing_adjustment.py
+++ multiple_testing_adjustment.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 ################################################################################
-# DATE: 2016/August/23
+# DATE: 2017/03/08
 # 
 # MODULE: adjustPval.py
 #
-# VERSION: 1.0
+# VERSION: 1.1
 # 
 # AUTHOR: Miguel Ibarra (miguelib@ufl.edu) 
 #
@@ -24,8 +24,8 @@ import statsmodels.sandbox.stats.multicomp as stm
 
 # Local Packages
 import logger as sl
-from interface import wideToDesign
 from flags import Flags
+from interface import wideToDesign
 
 def getOptions():
     """Function to pull arguments"""
@@ -33,40 +33,48 @@ def getOptions():
                                     " dataset and calculates the LOD on it ")
 
     #Standar input for SECIMtools
-    standar = parser.add_argument_group(title='Standard input', description= 
-                                        'Standard input for SECIM tools.')
+    standar = parser.add_argument_group(title='Standard input', 
+                        description='Standard input for SECIM tools.')
     standar.add_argument("-i","--input",dest="input", action='store', 
                         required=True, help="Input dataset in wide format.")
     standar.add_argument("-id","--uniqID",dest="uniqID",action="store",
                         required=True, help="Name of the column with unique" \
                         "dentifiers.")
-    standar.add_argument("-pv","--pval",dest="pval",action="store",
+
+    # tool input
+    tool = parser.add_argument_group(title='Tool input', 
+                        description='Specific input for the tool.')
+    tool.add_argument("-pv","--pval",dest="pval",action="store",
                         required=True, help="Name of the column with p-value.")
-    standar.add_argument("-a","--alpha",dest="alpha",action="store",type=float,
+    tool.add_argument("-a","--alpha",dest="alpha",action="store",type=float,
                         required=False,default=0.05, help="Alpha value.")
 
     #Output Paths
-    output = parser.add_argument_group(title='Output paths', description=
-                                       "Paths for the output files")
+    output = parser.add_argument_group(title='Output paths', 
+                        description="Paths for the output files")
     output.add_argument("-on","--outadjusted",dest="outadjusted",action="store",
                         required=True,help="Output path for corrected file[TSV]")
     output.add_argument("-fl","--flags",dest="flags",action="store",
                         required=True,help="Output path for flags file[TSV]")
-
     args = parser.parse_args()
+
+    # Standardize paths
+    args.input       = os.path.abspath(args.input)
+    args.flags       = os.path.abspath(args.flags)
+    args.outadjusted = os.path.abspath(args.outadjusted)
+
     return (args)
 
 def main(args):
-    # Importing data trough
+    # Reading input data, set "rowID" as index and just the pval column
+    logger.info("Importing data")
     toCorrect_df = pd.read_csv(args.input, sep="\t")
-
-    # Set rowID as indexes
     toCorrect_df.set_index(args.uniqID, inplace=True)
-
-    # select just pvals
     justPvals = toCorrect_df[args.pval].values
 
     # Making bonferroni, Benjamini/Hochberg, Benjamini/Yekutieli
+    # Alpha for FWER, family-wise error rate, e.g. 0.1
+    # http://statsmodels.sourceforge.net/devel/generated/statsmodels.sandbox.stats.multicomp.multipletests.html
     logger.info("Runnig corrections")
     bonferroni = pd.Series(stm.multipletests(justPvals,alpha=args.alpha,
                     returnsorted=False, method="bonferroni")[1],
@@ -78,23 +86,22 @@ def main(args):
                     returnsorted=False, method="fdr_by")[1],
                     name=args.pval+"_bYekutieli", index=toCorrect_df.index)
 
-    #Creating objet with flags
+    # Creating objet with flags
+    # Add a column for each correction
+    logger.info("Getting Flags")
     significance_flags = Flags(index=toCorrect_df.index)
-
-    #Add a column for each correction
     for test in [bonferroni,bHochberg,bYekutieli]:
         significance_flags.addColumn(column="flag_{0}_off".format(test.name),
-            mask=(test>args.alpha))
+                                     mask=(test>args.alpha))
     
     # Concatenating results with pvals
     results = pd.concat([toCorrect_df[args.pval],bonferroni,bHochberg,bYekutieli],axis=1)
 
     # Saving data
-    logger.info("Saving results")
-    results.to_csv(os.path.abspath(args.outadjusted),sep="\t")
-
-    logger.info("Saving flags")
-    significance_flags.df_flags.to_csv(os.path.abspath(args.flags),sep="\t")
+    logger.info("Saving results and flags")
+    results.to_csv(args.outadjusted,sep="\t")
+    significance_flags.df_flags.to_csv(args.flags,sep="\t")
+    logger.info("Script Complete!")
 
 if __name__ == '__main__':
     # Import data
@@ -103,11 +110,10 @@ if __name__ == '__main__':
     # Setting logger
     logger = logging.getLogger()
     sl.setLogger(logger)
-    logger.info(u"""Importing data with following parameters:
-                Input: {0}
-                uniqID: {1}
-                pVal: {2}
-                """.format(args.input, args.uniqID, args.pval))
+    logger.info("Importing data with following parameters:"\
+                "\n\tInput: {0}"\
+                "\n\tuniqID: {1}"\
+                "\n\tpVal: {2}".format(args.input, args.uniqID, args.pval))
 
     # Start main
     main(args)
