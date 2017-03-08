f87987ea963ef101cda84373cf288c3c79723b7f
miguelib <ibarrarellano@gmail.com>
2017-03-08 15:48:25 -0500

    Logger added to the interface

--- threshold_based_flags.py
+++ threshold_based_flags.py
@@ -1,89 +1,90 @@
 #!/usr/bin/env python
 
 # Built-in packages
-import argparse
+import os
 import logging
+import argparse
+
 # Local packages
-from interface import wideToDesign
-from flags import Flags
 import logger as sl
+from flags import Flags
+from interface import wideToDesign
 
 
 def getOptions():
     """Function to pull in arguments"""
-    description = """"""
+    description = """Flags bhased on treshold"""
     parser = argparse.ArgumentParser(description=description)
-
-    group1 = parser.add_argument_group(title='Standard input', 
+    # Standard input
+    standard = parser.add_argument_group(title='Standard input', 
             description='Standard input for SECIM tools.')
-    group1.add_argument("-i", "--input", dest="fname", action='store', 
+    standard.add_argument("-i", "--input", dest="input", action='store', 
                         required=True, help="Input dataset in wide format.")
-    group1.add_argument("-d", "--design", dest="dname", action='store', 
+    standard.add_argument("-d", "--design", dest="design", action='store', 
                         required=True, help="Design file.")
-    group1.add_argument("-id", "--ID", dest="uniqID", action='store', 
+    standard.add_argument("-id", "--ID", dest="uniqID", action='store', 
                         required=True, help="Name of the column with unique:"\
                         " identifiers.")
-    group1.add_argument("-o", "--output", dest="output", action='store', 
-                        required=True, help="Output path for the created "\
-                        "flag file.")
-    group1.add_argument("-g", "--group", dest="group", action='store', 
+    standard.add_argument("-g", "--group", dest="group", action='store', 
                         required=True, default=None, help="Add the option to "\
                         "separate sample IDs by treatement name. ")
-
-    group2 = parser.add_argument_group(title='Optional input', 
-            description='Optional input for On/Off tool.')
-    group2.add_argument("-c", "--cutoff", dest="cutoff", action='store', 
+    # Tool especific input
+    tool = parser.add_argument_group(title='Tool input', 
+            description='Tool specific input.')
+    tool.add_argument("-c", "--cutoff", dest="cutoff", action='store', 
                         required=False, default=30000, type=int, 
                         help="Cutoff to use for which values to flag. This "\
                         "defaults to 30,000")
-
+    # Output
+    output = parser.add_argument_group(title='Tool output', 
+            description='Tool output paths.')
+    output.add_argument("-o", "--output", dest="output", action='store', 
+                        required=True, help="Output path for the created "\
+                        "flag file.")
     args = parser.parse_args()
+
+    # Standardize paths
+    args.input  = os.path.abspath(args.input)
+    args.design = os.path.abspath(args.design)
+    args.output = os.path.abspath(args.output)
 
     return(args)
 
 
 def main(args):
-
     # Import data
-    dat = wideToDesign(args.fname, args.dname, args.uniqID)
+    logger.info("Importing data with the interface")
+    dat = wideToDesign(args.input, args.design, args.uniqID)
 
-    df_offFlags = Flags(index=dat.wide.index)
-    logger.info("Running On/Off")
+
     # Iterate through each group to add flags for if a group has over half of
     # its data above the cutoff
+    logger.info("Running threshold based flags")
+    df_offFlags = Flags(index=dat.wide.index)
     for title, group in dat.design.groupby(args.group):
-        # Create mask of current frame containing True/False values if the
-        # values are greater than the cutoff
-        mask = (dat.wide[group.index] < args.cutoff)
-
-        # Convert the mean column to a boolean
+        mask   = (dat.wide[group.index] < args.cutoff)
         meanOn = mask.mean(axis=1)
-
-        # Add mean column of boolean values to flags
         df_offFlags.addColumn(column='flag_feature_' + title + '_off', 
-                            mask=meanOn > 0.5)
-
-    # flag_met_off column
-    maskFlagMetOff = df_offFlags.df_flags.any(axis=1)
-    df_offFlags.addColumn('flag_feature_off', maskFlagMetOff)
-
-    # flag_met_all_off column
-    maskFlagMetAllOff = df_offFlags.df_flags.all(axis=1)
-    df_offFlags.addColumn('flag_feature_all_off', maskFlagMetAllOff)
+                              mask=meanOn > 0.5)
 
     logger.info("Creating output")
     df_offFlags.df_flags.to_csv(args.output, sep="\t")
 
 if __name__ == "__main__":
-
     # Command line options
     args = getOptions()
 
+    # Setting logger
     logger = logging.getLogger()
-
     sl.setLogger(logger)
 
+    # Starting script
+    logger.info("Importing data with following parameters:"\
+                "\n\tInput: {0}"\
+                "\n\tDesign: {1}"\
+                "\n\tuniqID: {2}"\
+                "\n\tgroup: {3}".format(args.input, args.design, args.uniqID, 
+                                args.group))
     # Plotting import info 
     logger.info('Importing Data')
     main(args)
-
