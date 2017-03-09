6d2c61e3ba2f0e5f8b889aced47b943a4bd74a64
miguelib <ibarrarellano@gmail.com>
2017-03-09 10:27:55 -0500

    new logs added

--- summarize_flags.py
+++ summarize_flags.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 ################################################################################
+# DATE: 2017/03/09
+#
 # SCRIPT: summarizeFlags.py
 # 
 # AUTHOR: Miguel Ibarra Arellano (miguelib@ufl.edu)
 # 
 # DESCRIPTION: This script takes a f;ag format file (wide), a flag file and a 
 # unique ID for that file.  And its going to create a summary of the flags 
-# calculating the sum, mean, any and all. Any wil tell you if any of the flags is
-# on and all will tell you if all the flags are on.
+# calculating the sum, mean, any and all. Any wil tell you if any of the flags 
+# is on and all will tell you if all the flags are on.
 #
 # INPUT: 
 #       Flag file:
@@ -21,6 +23,7 @@
 ################################################################################
 
 #Standard Libraries
+import os
 import logging
 import argparse
 from argparse import RawDescriptionHelpFormatter
@@ -29,58 +32,53 @@ from argparse import RawDescriptionHelpFormatter
 import pandas as pd
 
 #Local Libraries
-from interface import wideToDesign
-from flags import Flags
 import logger as sl
+from flags import Flags
+from interface import wideToDesign
 
-
-
-"""Function to pull arguments"""
 def getOptions():
+    """Function to pull arguments"""
     parser = argparse.ArgumentParser(description="Drops rows or columns given "\
                                     "an specific cut value and condition")
     
     required = parser.add_argument_group(title="Required Input", 
                                         description="Requiered input to the "\
                                         "program.")
+    # Required input
     required.add_argument('-f',"--flags",dest="flagFile", action="store",
                         required=True,help="Flag file.")
     required.add_argument('-id',"--ID",dest="uniqID",action="store",
                         required=True,help="Name of the column with unique "\
                         "identifiers.")
-
+    # Outptu
     output = parser.add_argument_group(title="Output", description="Output of "\
                                         "the script.")
     output.add_argument('-os',"--outSummary",dest="outSummary",action="store",
-                        required=True,help="Output file for Drops.")
-
+                        required=True,help="Output file for Summary.")
     args = parser.parse_args()
+
+    # Standardize paths
+    args.flagFile   = os.path.abspath(args.flagFile)
+    args.outSummary = os.path.abspath(args.outSummary)
+
     return(args);
 
 def main(args):
     # Convert flag file to DataFrame
-    flags_df = pd.DataFrame.from_csv(args.flagFile, sep='\t')
+    df_inp_flags = pd.DataFrame.from_csv(args.flagFile, sep='\t')
 
     #Creating flag object
-    offFlags_df = Flags(index=flags_df.index)
+    offFlags_df = Flags(index=df_inp_flags.index)
 
-    #Get sum flags colum
-    flagSum = flags_df.sum(axis=1)
-    offFlags_df.addColumn("flag_sum",flagSum)
-
-    #Get mean flags column
-    offFlags_df.flags_df.loc[:,"flag_mean"] = flags_df.mean(axis=1)
-
-    #Calculate flag at least one off
-    maskAny = flags_df.any(axis=1)
-    offFlags_df.addColumn("flag_any_off", maskAny)
-
-    #Calculate flag all off
-    maskAll = flags_df.all(axis=1)
-    offFlags_df.addColumn("flag_all_off", maskAll)
+    #Get flags for sum,mean, any and all
+    logger.info("Creating flags")
+    offFlags_df.addColumn("flag_sum",df_inp_flags.sum(axis=1))
+    offFlags_df.df_flags.loc[:,"flag_mean"]=df_inp_flags.mean(axis=1)
+    offFlags_df.addColumn("flag_any_off", df_inp_flags.any(axis=1))
+    offFlags_df.addColumn("flag_all_off", df_inp_flags.all(axis=1))
 
     #Concatenate flags and summary flags
-    offFlags_df = pd.concat([flags_df,offFlags_df.flags_df],axis=1)
+    offFlags_df = pd.concat([df_inp_flags,offFlags_df.df_flags],axis=1)
 
     #Output flags
     offFlags_df.to_csv(args.outSummary, sep='\t')
