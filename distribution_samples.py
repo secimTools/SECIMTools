6759a12c4c078214f8adeae9c4680eacf34b3dfe
miguelib <ibarrarellano@gmail.com>
2017-03-10 15:38:30 -0500

    flexibility on the palette use of levels and logger to the interface

--- distribution_samples.py
+++ distribution_samples.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
-######################################################################################
+################################################################################
 # Date: 2016/July/06 ed. 1016/July/11
 # 
 # Module: distribution_samples.py
 #
 # VERSION: 1.1
 # 
-# AUTHOR: Miguel Ibarra (miguelib@ufl.edu) Edited by Matt Thoburn (mthoburn@ufl.edu)
+# AUTHOR: Miguel Ibarra (miguelib@ufl.edu) 
+#         Edited by Matt Thoburn (mthoburn@ufl.edu)
 #
-# DESCRIPTION: This program creates a distribution plot by samples for a given datset
+# DESCRIPTION: This program creates a distribution plot by samples for a given 
+#              datset
 #
-#######################################################################################
+################################################################################
 
 # Built-in packages
 import os
@@ -19,7 +21,6 @@ import argparse
 from argparse import RawDescriptionHelpFormatter
 
 # Add-on packages
-import mpld3
 import matplotlib
 matplotlib.use('Agg')
 import numpy as np
@@ -40,38 +41,48 @@ def getOptions():
     description = """ Distribution Analysis: Plot sample distrubtions. """
     parser = argparse.ArgumentParser(description=description, 
                                     formatter_class=RawDescriptionHelpFormatter)
-    standar = parser.add_argument_group(title="Standar input", description="""
-                                        Standar input for SECIM tools.""")
-    standar.add_argument("-i","--input", dest="fname", action='store', 
+    standard = parser.add_argument_group(title="Standar input", 
+        description="Standar input for SECIM tools.")
+    standard.add_argument("-i","--input", dest="input", action='store', 
                         required=True, help="Input dataset in wide format.")
-    standar.add_argument("-d","--design", dest="dname", action='store', 
+    standard.add_argument("-d","--design", dest="design", action='store', 
                         required=True, help="Design file.")
-    standar.add_argument("-id","--ID", dest="uniqID", action='store', 
+    standard.add_argument("-id","--ID", dest="uniqID", action='store', 
                         required=True, help="Name of the column with uniqueID.")
+    standard.add_argument("-g","--group", dest="group", action='store', 
+                        required=False, default=False, help="Name of column in "\
+                        "design file with Group/treatment information.")
+    standard.add_argument("-o",'--order', dest='order', action='store', 
+                        required=False, default=False, help="Name of the column "\
+                        "with the runOrder")
+    standard.add_argument("-l","--levels",dest="levels",action="store", 
+                        required=False, default=False, help="Different groups to"\
+                         "sort by separeted by commas.")
 
     output = parser.add_argument_group(title="Output paths", description="""Paths 
                                         and outputs""")
     output.add_argument("-f","--figure",dest="figure",action="store",required=True,
                         help="Path for the distribution figure")
 
-    optional = parser.add_argument_group(title="Optional input", 
-                        description="Optional input for districution script.")
-    optional.add_argument("-g","--group", dest="group", action='store', 
-                        required=False, default=False, help="""Name of column in 
-                        design file with Group/treatment information.""")
-    optional.add_argument("-o",'--order', dest='order', action='store', 
-                        required=False, default=False, help="""Name of the column 
-                        with the runOrder""")
-    optional.add_argument("-l","--levels",dest="levels",action="store", 
-                        required=False, default=False, help="""Different groups to
-                         sort by separeted by commas.""")
 
+    plot = parser.add_argument_group(title='Plot options')
+    plot.add_argument("-pal","--palette",dest="palette",action='store',required=False, 
+                        default="tableau", help="Name of the palette to use.")
+    plot.add_argument("-col","--color",dest="color",action="store",required=False, 
+                        default="Tableau_20", help="Name of a valid color scheme"\
+                        " on the selected palette")
     args = parser.parse_args()
+
+    # Standardize paths
+    args.figure = os.path.abspath(args.figure)
+
+    # if args.levels then split otherwise args.level = emptylist
+    if args.levels:
+        args.levels = args.levels.split(",")
     return(args)
 
-def plotDensityDistribution(pdf,wide,design,uGroups,lvlName):
+def plotDensityDistribution(pdf,wide,palette):
     # Instanciating figureHandler object
-    logger.info(u"Plotting density for sample distribution")
     figure = figureHandler(proj="2d", figsize=(12,7))
 
     # Formating axis
@@ -79,10 +90,10 @@ def plotDensityDistribution(pdf,wide,design,uGroups,lvlName):
                         ylim="ignore",grid=False)
 
     # Plotting density plot
-    density.plotDensityDF(colors=design["colors"],ax=figure.ax[0],data=wide)
+    density.plotDensityDF(colors=palette.design["colors"],ax=figure.ax[0],data=wide)
 
     # Add legend to the plot
-    figure.makeLegend(ax=figure.ax[0], ucGroups=uGroups, group=lvlName)
+    figure.makeLegend(ax=figure.ax[0], ucGroups=palette.ugColors, group=palette.combName)
 
     # Shrinking figure
     figure.shrink()
@@ -90,9 +101,8 @@ def plotDensityDistribution(pdf,wide,design,uGroups,lvlName):
     # Adding to PDF
     figure.addToPdf(pdf, dpi=600)
 
-def plotBoxplotDistribution(pdf,wide,design):
+def plotBoxplotDistribution(pdf,wide,palette):
     # Instanciating figureHandler object
-    logger.info(u"Plotting boxplot for sample distribution")
     figure = figureHandler(proj="2d",figsize=(max(len(wide.columns)/4,12),7))
 
     # Formating axis
@@ -100,7 +110,7 @@ def plotBoxplotDistribution(pdf,wide,design):
                     grid=False,xlim="ignore")
 
     # Plotting boxplot
-    box.boxDF(ax=figure.ax[0],colors=design["colors"],dat=wide)
+    box.boxDF(ax=figure.ax[0],colors=palette.design["colors"],dat=wide)
 
     # Shrinking figure
     plt.tight_layout()
@@ -112,64 +122,43 @@ def main(args):
     """
     Function to call all other functions
     """
-    logger.info(u"""Importing data with following parameters: 
-                \tWide: {0}
-                \tDesign: {1}
-                \tUnique ID: {2}
-                \tGroup: {3}
-                \tRun Order: {4}
-                \tLevels: {5}
-                """ .format(args.fname, args.dname, args.uniqID, args.group, 
-                    args.order,args.levels))
-
     # Checking if levels
-    subGroups = []
     if args.levels and args.group:
-        subGroups = args.levels.split(",")
-        levels = [args.group]+subGroups
+        levels = [args.group]+args.levels
     elif args.group and not args.levels:
         levels = [args.group]
-    elif not args.group and args.levels:
-        logger.error(u"Use --groups if you are using one group to select")
-        exit()
     else:
         levels = []
-    logger.info(u"Groups used to color by :{}".format(",".join(levels)))
-
-    # Checkig if order
-    if args.order and args.levels:
-        anno = [args.order] + subGroups
-    elif args.order and not args.levels:
-        anno = [args.order,]
-    elif not args.order and args.levels:
-        anno = [args.levels,]
-    else:
-        anno=False
+    logger.info(u"Groups used to color by: {0}".format(",".join(levels)))
 
     # Parsing files with interface
-    dat = wideToDesign(args.fname,args.dname,args.uniqID,args.group,anno=anno)
-    logger.info(u"Data loaded by interface")
+    logger.info(u"Loading data with the Interface")
+    dat = wideToDesign(args.input, args.design, args.uniqID, args.group,
+                    anno=args.levels, runOrder=args.order, logger=logger)
+
+    # Cleaning from missing data
+    #dat.dropMissing()
     
-    # Sort data if runOrder
+    # Sort data by runOrder if provided
     if args.order:
-        dat.design.sort_values(by=args.order,inplace=True)
-        dat.wide = dat.wide[dat.design.index.values]
-        logger.info(u"Sorting by runOrderf")
+        logger.info(u"Sorting by runOrder")
+        dat.sortByRunOrder()
 
     # Get colors for each sample based on the group
-    dat.design,uGroups,combName=palette.getColors(design=dat.design,groups=levels)
+    palette.getColors(design=dat.design,groups=levels)
 
     # Open PDF pages to output figures
-    with PdfPages(os.path.abspath(args.figure)) as pdf:
+    with PdfPages(args.figure) as pdf:
+
         # Plot density plot
-        plotDensityDistribution(pdf=pdf,wide=dat.wide,design=dat.design,
-                                uGroups=uGroups, lvlName=combName)
+        logger.info(u"Plotting density for sample distribution")
+        plotDensityDistribution(pdf=pdf, wide=dat.wide, palette=palette)
 
         # Plot boxplots
-        plotBoxplotDistribution(pdf=pdf,wide=dat.wide,design=dat.design)
+        logger.info(u"Plotting boxplot for sample distribution")
+        plotBoxplotDistribution(pdf=pdf, wide=dat.wide, palette=palette)
         
-    logger.info(u"Ending script")
-
+    logger.info(u"Script complete!")
 
 if __name__ == '__main__':
     # import Arguments
@@ -179,9 +168,20 @@ if __name__ == '__main__':
     logger = logging.getLogger()
     sl.setLogger(logger)
 
-    # Set palette of colors
-    palette = colorHandler(pal="tableau", col="Tableau_10")
-    logger.info(u"Using tableau Tableau_10 palette")
+    # Print logger info
+    logger.info(u"""Importing data with following parameters: 
+            \tWide: {0}
+            \tDesign: {1}
+            \tUnique ID: {2}
+            \tGroup: {3}
+            \tRun Order: {4}
+            \tLevels: {5}
+            """ .format(args.input, args.design, args.uniqID, args.group, 
+                args.order,args.levels))
 
+    # Set color palette
+    palette = colorHandler(pal=args.palette, col=args.color)
+    logger.info(u"Using {0} color scheme from {1} palette".format(args.color,
+                args.palette))
     # main
     main(args)
