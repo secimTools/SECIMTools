3d6a90dfb290789c810eaca1b4f0389e73b9d1bd
miguelib <ibarrarellano@gmail.com>
2017-03-13 09:24:36 -0400

    logger added to the interface, plots now use flexible palette

--- partial_least_squares.py
+++ partial_least_squares.py
@@ -60,10 +60,16 @@ def getOptions(myopts=None):
     standard.add_argument("-g", "--group",dest="group", action='store',
                         required=True, default=False, help="Name of the column"\
                         " with groups.")
-    standard.add_argument("-t", "--toCompare",dest="toCompare", action='store', 
+    standard.add_argument("-l","--levels",dest="levels",action="store", 
+                        required=False, default=False, help="Different groups to"\
+                        " sort by separeted by commas.")
+
+    tool = parser.add_argument_group(title='Tool specific input', 
+                                description='Input specific for this tool.')
+    tool.add_argument("-t", "--toCompare",dest="toCompare", action='store', 
                         required=True, default=True, help="Name of"
                         " the elements to compare in group col.")
-    standard.add_argument("-n", "--nComp",dest="nComp", action='store', 
+    tool.add_argument("-n", "--nComp",dest="nComp", action='store', 
                         required=False,  default=3, type = int, help="Number"\
                         " of components.")
 
@@ -75,40 +81,31 @@ def getOptions(myopts=None):
     output.add_argument("-f","--figure",dest="figure",action="store",required=False,
                         help="Name of output file to store scatter plots for scores")
 
+    plot = parser.add_argument_group(title='Plot options')
+    plot.add_argument("-pal","--palette",dest="palette",action='store',required=False, 
+                        default="tableau", help="Name of the palette to use.")
+    plot.add_argument("-col","--color",dest="color",action="store",required=False, 
+                        default="Tableau_20", help="Name of a valid color scheme"\
+                        " on the selected palette")
+
     development = parser.add_argument_group(title='Development Settings')
     development.add_argument("--debug",dest="debug",action='store_true',
                         required=False,help="Add debugging log output.")
     args = parser.parse_args()
+
+    # Standardize Paths
+    args.figure     = os.path.abspath(args.figure)
+    args.outScores  = os.path.abspath(args.outScores)
+    args.outWeights = os.path.abspath(args.outWeights)
+
+    # Split levels if levels
+    if args.levels:
+        args.levels = args.levels.split(",")
+
+    # Split to compare
+    args.toCompare = args.toCompare.split(",")
+
     return(args)
-
-def dropMissing(wide):
-    """
-    Drops missing data out of the wide file
-
-    :Arguments:
-        :type wide: pandas.core.frame.DataFrame
-        :param wide: DataFrame with the wide file data
-
-    :Returns:
-        :rtype wide: pandas.core.frame.DataFrame
-        :return wide: DataFrame with the wide file data without missing data
-    """
-    #Warning
-    logger.warn("Missing values were found")
-
-    #Count of original
-    nRows = len(wide.index)      
-
-    #Dropping 
-    wide.dropna(inplace=True)    
-
-    #Count of dropped
-    nRowsNoMiss = len(wide.index)  
-
-    #Warning
-    logger.warn("{} rows were dropped because of missing values.".
-                format(nRows - nRowsNoMiss))
-    return wide
 
 def runPLS(trans, group,toCompare,nComp):
     """
@@ -144,7 +141,7 @@ def runPLS(trans, group,toCompare,nComp):
     plsr.fit(subset.values,Y)
 
     # Creating column names for scores and weights
-    LVNames = ["LV_{0}".format(i) for i in range(nComp)]
+    LVNames = ["LV_{0}".format(i+1) for i in range(nComp)]
 
     # The scores describe the position of each sample in each determined LV.
     # Columns for LVs and rows for Samples. (These are the ones the you plot).
@@ -159,83 +156,15 @@ def runPLS(trans, group,toCompare,nComp):
     # Returning the results
     return scores_df,weights_df
 
-def plotScatterplot(data,fh,x,y,design,title,group=False):
-    """
-    Creates an scater plot for 2 given components. It returns the ax with
-    the figure.
-    :Arguments:
-        :type data: pandas.core.frame.DataFrame
-        :param data: Data frame with the data to plot.
-
-        :type ax: ax:matplotlib.axes._subplots.AxesSubplot.
-        :param ax: empty axes.
-
-        :type x: string
-        :param x: Name of the first set of data.
-
-        :type y: string
-        :param y: name of the second set of data.
-
-        :type design: interface.wideToDesign object.
-        :param design: All standard data parsed
-
-        :type title: string
-        :param title: title for axis
-
-        :type group: string
-        :param group: Name of the column that contains the group information on the design file.
-
-    :Return:
-        :rtype fh: figureHandler.
-        :returns ax: figureHandler containing ax with the scatter plot and legend.
-
-    """
-    logger.info(u"Creating scatter plot for components {0} vs {1} ".
-                format(x,y))
-    if group:
-        glist=list(design[group])
-        ch = colorHandler("tableau","Tableau_20")
-        colorList, ucGroups = ch.getColorsByGroup(design=design,group=group,uGroup=sorted(set(glist)))
-    else:
-        glist = list()
-        colorList = list('b') #blue
-        ucGroups = dict()
-    
-    scatter.scatter2D(ax=fh.ax[0],x=list(data[x]),y=list(data[y]),colorList=colorList)
-
-    fh.despine(fh.ax[0])
-
-    if group:
-        fh.makeLegend(ax=fh.ax[0],ucGroups=ucGroups,group=group)
-        fh.shrink()
-
-    #Get Variance
-    fh.formatAxis(figTitle=title,xTitle="Scores on {0}".format(x),
-        yTitle="Scores on {0}".format(y),grid=False)
-
-    return fh
-
-def plotScores(scores,pdf,dat):
+def plotScores(data, palette, pdf):
     """
     This function creates a PDF file with 3 scatter plots for the combinations 
     of the 3 principal components. PC1 vs PC2, PC1 vs PC3, PC2 vs PC3.
 
     :Arguments:
-        :type dat: interface.wideToDesign object.
-        :param dat: All standard data parsed
-
         :type data: pandas.core.frame.DataFrame
         :param data: Data frame with the data to plot.
         
-        :type PC1: string
-        :param PC1:Name of the first set of data.
-
-        :type PC2: string
-        :param PC2:Name of the second set of data.
-
-        :type PC3: string
-        :param PC3:Name of the third set of data.
-
         :type outpath: string
         :param outpath: Path for the output file
 
@@ -246,50 +175,73 @@ def plotScores(scores,pdf,dat):
         :rtype PDF: file
         :retrn PDF: file with the 3 scatter plots for PC1 vs PC2, PC1 vs PC3, PC2  vs PC3.
     """
-    for LV1,LV2 in  list(itertools.combinations(scores.columns.tolist(),2)):
+    for x,y in  list(itertools.combinations(data.columns.tolist(),2)):
         # Creating a figure handler object
-        sFig = figureHandler(proj="2d", figsize=(14,8))
+        fh = figureHandler(proj="2d", figsize=(14,8))
 
         # Creating title for the figure
-        title = "{0} vs {1}".format(LV1,LV2)
+        title = "{0} vs {1}".format(x,y)
+        
+        # Creating the scatterplot 2D
+        scatter.scatter2D(ax=fh.ax[0], x=list(data[x]), y=list(data[y]),
+                        colorList=palette.design.colors.tolist())
 
-        # Plotting
-        fh1 = plotScatterplot(scores,sFig,LV1,LV2,dat.design.loc[scores.index,:],
-                            title,dat.group)
+        # Despine axis
+        fh.despine(fh.ax[0])
+
+        # Print Legend
+        fh.makeLegend(ax=fh.ax[0], ucGroups=palette.ugColors, group=palette.combName)
+
+        # Shinking the plot so everything fits
+        fh.shrink()
+
+        # Format Axis
+        fh.formatAxis(figTitle=title, xTitle="Scores on {0}".format(x),
+                    yTitle="Scores on {0}".format(y), grid=False)
 
         # Adding figure to pdf
-        fh1.addToPdf(dpi=90,pdfPages=pdf)
+        fh.addToPdf(dpi=90,pdfPages=pdf)
 
 def main(args):
-    # Splitting groups to compare
-    toCompare = args.toCompare.split(",")
+    # Checking if levels
+    if args.levels and args.group:
+        levels = [args.group]+args.levels
+    elif args.group and not args.levels:
+        levels = [args.group]
+    else:
+        levels = []
 
     # Loading data trought Interface
-    dat = wideToDesign(args.input, args.design, args.uniqID,group=args.group)
+    dat = wideToDesign(args.input, args.design, args.uniqID, group=args.group, 
+                        anno=args.levels, logger=logger)
 
     # Treat everything as numeric
     dat.wide = dat.wide.applymap(float)
 
-    #Dropping missing values
-    if np.isnan(dat.wide.values).any():
-        dat.wide = dropMissing(dat.wide)
+    # Cleaning from missing data
+    dat.dropMissing()
+
+    # Get colors for each sample based on the group
+    palette.getColors(design=dat.design, groups=levels)
 
     # Transpose data
     dat.trans = dat.transpose()
 
-    #Run PLS
-    scores_df,weights_df = runPLS(dat.trans,dat.group,toCompare,args.nComp)
+    # Run PLS
+    df_scores,df_weights = runPLS(dat.trans,dat.group,args.toCompare,args.nComp)
 
-    # Save scores_df
-    scores_df.to_csv(args.outScores,sep="\t", index_label='sampleID')
-    
-    # Save weights_df
-    weights_df.to_csv(args.outWeights,sep="\t", index_label=dat.uniqID)
+    # Update palette afterdrop selection of groups toCompare
+    palette.design   =  palette.design.T[df_scores.index].T
+    palette.ugColors =  {ugc:palette.ugColors[ugc] for ugc in palette.ugColors.keys() if ugc in args.toCompare}
 
     # Plotting scatter plot for scores
     with PdfPages(args.figure) as pdfOut:
         logger.info(u"Plotting PLS scores")
-        plotScores(scores=scores_df,pdf=pdfOut,dat=dat)
+        plotScores(data=df_scores, palette=palette, pdf=pdfOut)
+
+    # Save df_scores and df_weights  to tsv files
+    df_scores.to_csv(args.outScores,  sep="\t", index_label='sampleID')
+    df_weights.to_csv(args.outWeights,sep="\t", index_label=dat.uniqID)
 
     #Ending script
     logger.info(u"Finishing running of PLS")
@@ -312,4 +264,11 @@ if __name__ == '__main__':
                 uniqID: {2}
                 group: {3}
                 """.format(args.input, args.design, args.uniqID, args.group))
+
+    # Stablishing color palette
+    palette = colorHandler(pal=args.palette, col=args.color)
+    logger.info(u"Using {0} color scheme from {1} palette".format(args.color,
+                args.palette))
+
+    # running main script
     main(args)
