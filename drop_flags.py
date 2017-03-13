84d80423026351108a1ac8118acfd7c4075916b3
miguelib <ibarrarellano@gmail.com>
2017-03-13 10:27:27 -0400

    re-arrenge drop flags code

--- drop_flags.py
+++ drop_flags.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 ################################################################################
-# SCRIPT: dropFlag_v2.1.py
+# SCRIPT: drop_flags.1.py
 # 
 # LAST VERSION: Includes support to select the type of drop to perform instead
 #               of automatically decide. Also includes better recognition 
@@ -24,6 +24,7 @@
 ################################################################################
 
 #Standard Libraries
+import os
 import re
 import copy
 import logging
@@ -42,45 +43,52 @@ from interface import wideToDesign
 def getOptions():
     parser = argparse.ArgumentParser(description="Drops rows or columns given" \
                                     "an specific cut value and condition.")
-    
-    required = parser.add_argument_group(title='Required Input', 
-                                        description="Requiered input to the "\
-                                        "program.")
-    required.add_argument('-i',"--input",dest="input", action="store",
+    # Standard input
+    standard = parser.add_argument_group(title='Required Input', 
+                        description="Standard inputs for SECIM tools.")
+    standard.add_argument('-i',"--input",dest="input", action="store",
                         required=True,help="Input dataset in wide format.")
-    required.add_argument('-d',"--design",dest="designFile", action="store",
+    standard.add_argument('-d',"--design",dest="design", action="store",
                         required=True, help="Design file.")
-    required.add_argument('-f',"--flags",dest="flagFile", action="store",
-                        required=True,help="Flag file.")
-    required.add_argument('-id',"--ID",dest="uniqID",action="store",
+    standard.add_argument('-id',"--ID",dest="uniqID",action="store",
                         required=True,help="Name of the column with unique "\
                         "identifiers.")
-    required.add_argument('-fid',"--flagUniqID",dest="flagUniqID",action="store",
+    # Tool specific input
+    tool = parser.add_argument_group(title="Tool Input", 
+                        description="Tool especific input.")
+    tool.add_argument('-f',"--flags",dest="flags", action="store",
+                        required=True,help="Flag file.")
+    tool.add_argument('-fft',"--flagfiletype",dest="flagfiletype",action='store',
+                        required=True, default="row", choices=["row","column"],
+                        help="Type of flag file")   
+    tool.add_argument('-fid',"--flagUniqID",dest="flagUniqID",action="store",
                         required=False, default=False,help="Name of the column "\
                         "with unique identifiers in the flag files.")
-    required.add_argument('-fd',"--flagDrop",dest="flagDrop",action='store',
+    tool.add_argument('-fd',"--flagDrop",dest="flagDrop",action='store',
                         required=True, help="Name of the flag/field you want to"\
-                        " access.")
-
-    output = parser.add_argument_group(title='Output', description="Output of "\
-                                        "the script.")
+                        " access.")                                
+    tool.add_argument('-val',"--value",dest="value",action='store',
+                        required=False, default="1",help="Cut Value")
+    tool.add_argument('-con',"--condition",dest="condition",action='store',
+                        required=False, default="0",help="Condition for the cut" \
+                        "where 0=Equal to, 1=Greater than and 2=less than.")
+    # Tool output
+    output = parser.add_argument_group(title='Output', 
+                        description="Output of the script.")
     output.add_argument('-ow',"--outWide",dest="outWide",action="store",required=True,
                         help="Output file without the Drops.")
     output.add_argument('-of',"--outFlags",dest="outFlags",action="store",
                         required=True,help="Output file for Drops.")
 
-    optional = parser.add_argument_group(title="Optional Input", 
-                                    description="Optional Input to the program.")
-    optional.add_argument('-fft',"--flagfiletype",dest="flagfiletype",action='store',
-                        required=True, default="row", choices=["row","column"],
-                        help="Type of flag file")                                   
-    optional.add_argument('-val',"--value",dest="value",action='store',
-                        required=False, default="1",help="Cut Value")
-    optional.add_argument('-con',"--condition",dest="condition",action='store',
-                        required=False, default="0",help="Condition for the cut" \
-                        "where 0=Equal to, 1=Greater than and 2=less than.")
-
     args = parser.parse_args()
+
+    # Standardize paths
+    args.input    = os.path.abspath(args.input)
+    args.flags    = os.path.abspath(args.flags)
+    args.design   = os.path.abspath(args.design)
+    args.outWide  = os.path.abspath(args.outWide)
+    args.outFlags = os.path.abspath(args.outFlags)
+
     return(args);
 
 def dropRows(df_wide, df_flags,cut_value, condition, args):
@@ -144,11 +152,8 @@ def dropRows(df_wide, df_flags,cut_value, condition, args):
     # Use mas to drop values out of original flags
     df_flags_keeped = df_flags[mask_flags]
 
-    #Export wide
-    df_wide_keeped.to_csv(args.outWide, sep='\t')
-
-    #Export flags
-    df_flags_keeped.to_csv(args.outFlags, sep='\t')
+    # Returning datasets
+    return df_wide_keeped,df_flags_keeped
 
 def dropColumns(df_wide, df_flags,cut_value, condition, args):
     """ 
@@ -182,66 +187,56 @@ def dropColumns(df_wide, df_flags,cut_value, condition, args):
     if not(re.search(r'[a-zA-Z]+', cut_value)):
         cut_value = float(cut_value)
         if condition == '>':
-            samples_to_drop = df_flags.index[df_flags[args.flagDrop]<cut_value]
+            to_keep = df_flags.index[df_flags[args.flagDrop]<cut_value]
         elif condition == '<':
-            samples_to_drop = df_flags.index[df_flags[args.flagDrop]>cut_value]
+            to_keep = df_flags.index[df_flags[args.flagDrop]>cut_value]
         elif condition == '==':
-            samples_to_drop = df_flags.index[df_flags[args.flagDrop]!=cut_value]
+            to_keep = df_flags.index[df_flags[args.flagDrop]!=cut_value]
         else:
             logger.error(u'The {0} is not supported by the program, please use <,== or >'.format(condition))
             quit()
     else:
         cut_value = str(cut_value)
         if condition == '==':
-            samples_to_drop = df_flags.index[df_flags[args.flagDrop]!=cut_value]
+            to_keep = df_flags.index[df_flags[args.flagDrop]!=cut_value]
         else:
             logger.error(u'The {0} conditional is not supported for string flags, please use =='.format(condition))
             quit()
 
-    dropped_flags = df_flags.T[samples_to_drop].T
+    # Subsetting
+    dropped_flags = df_flags.T[to_keep].T
+    df_wide = df_wide[to_keep]
 
-    #Output 
-    df_wide.to_csv(args.outWide, columns=samples_to_drop, sep='\t')
-    dropped_flags.to_csv(args.outFlags, sep='\t')
+    # Returning
+    return df_wide,dropped_flags
 
 def main(args):
-    # Execute wideToDesign to make all data uniform
-    formatted_data = wideToDesign(wide=args.input, design=args.designFile, 
-                                uniqID=args.uniqID)    
+    # Import data with the interface
+    dat = wideToDesign(wide=args.input, design=args.design, 
+                    uniqID=args.uniqID, logger=logger)
 
-    # Convert flag file to DataFrame
-    df_flags = pd.read_table(args.flagFile)
+    # Read flag file
+    df_flags = pd.read_table(args.flags)
 
-    # Select index on flag file
-    # If user gives a flagUniqID the override automatic response
+    # Select index on flag file if none then rise an error
     if args.flagUniqID:
         df_flags.set_index(args.flagUniqID, inplace=True)
-    # If not flagUniqID then decides a default one based on type of drop if 
-    # column use 'sampleID'.
-    elif args.flagfiletype=="column":
-        df_flags.set_index("sampleID", inplace=True)
-    # If not flagUniqID then decides a default one based on type of drop if 
-    # row use 'rowID'.
-    elif args.flagfiletype=="row":
-        df_flags.set_index("rowID", inplace=True)
-    # Send error if none 
     else:
         logger.error("Not flagUniqID provided")
+        raise
 
     # Drop either rows or columns
+    logger.info("Running drop flags by {0}".format(args.flagfiletype))
     if args.flagfiletype=="column":
-        logger.info("Runing drop flags by Column")
-
-        # Running dropColumns routine
-        dropColumns(df_wide=formatted_data.wide, df_flags=df_flags, 
+        kpd_wide,kpd_flag = dropColumns(df_wide=dat.wide, df_flags=df_flags, 
                     cut_value=args.value, condition=args.condition, args=args)
-
     else:
-        logger.info("Runing drop flags by Row")
-
-        # Running dropRows routine
-        dropRows(df_wide=formatted_data.wide, df_flags=df_flags, 
+        kpd_wide,kpd_flag = dropRows(df_wide=dat.wide, df_flags=df_flags, 
                 cut_value=args.value, condition=args.condition, args=args)
+
+    # Wide and flags
+    kpd_wide.to_csv(args.outWide, sep='\t')
+    kpd_flag.to_csv(args.outFlags, sep='\t')
 
     # Finishing script
     logger.info("Script complete.")
@@ -272,7 +267,7 @@ if __name__ == '__main__':
         "\n\tVariable: {5}"\
         "\n\tCondition: {6}"\
         "\n\tValue: {7}"\
-        "\n\tType Of Flag File: {8}" .format(args.input,args.flagFile,args.designFile,
+        "\n\tType Of Flag File: {8}" .format(args.input,args.flags,args.design,
                             args.uniqID,args.outWide,args.outFlags,
                             args.condition,args.value,args.flagfiletype))
     #Main script
