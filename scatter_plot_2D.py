02f9692ca442ea2b9c4253c64d662f0045d83cae
miguelib <ibarrarellano@gmail.com>
2017-03-17 13:58:32 -0400

    re-code of scatterplot 2d and 3d, now they use visualManager

--- scatter_plot_2D.py
+++ scatter_plot_2D.py
@@ -1,115 +1,136 @@
-######################################################################################
+#!/usr/bin/env python
+################################################################################
 # DATE: 2016/August/10
 # 
-# MODULE: scatter3D.py
+# MODULE: scatter_plot_2D.py
 #
-# VERSION: 1.0
+# VERSION: 1.1
 # 
-# AUTHOR: Matt Thoburn (mthoburn@ufl.edu) ed. Miguel Ibarra (miguelib@ufl.edu)
+# AUTHOR: Matt Thoburn (mthoburn@ufl.edu), Miguel Ibarra (miguelib@ufl.edu)
 #
-# DESCRIPTION: This is a standalone executable for graphing 2D scatter plots from wide data
+# DESCRIPTION: This is a standalone executable for graphing 2D scatter plots 
+# from wide data
 #
-#######################################################################################
-from manager_color import colorHandler
-from manager_figure import figureHandler
-from interface import wideToDesign
-import module_scatter as scatter
-import pandas
-
+################################################################################
+# Import built-in libraries
+import os
+import logging
 import argparse
 from argparse import RawDescriptionHelpFormatter
+
+# Import add-on libraries
+import pandas as pd
+from matplotlib.backends.backend_pdf import PdfPages
+
+# Import local data libraries
+from dataManager import logger as sl
+from dataManager.interface import wideToDesign
+
+# Import local plotting libraries
+from visualManager import module_scatter as scatter
+from visualManager.manager_color import colorHandler
+from visualManager.manager_figure import figureHandler
 
 def getOptions():
     description="Stand alone function to plot Scatter Plots 2D"
     parser = argparse.ArgumentParser(description=description, 
-                                    formatter_class=RawDescriptionHelpFormatter)
-    standard = parser.add_argument_group(title='Standard input', 
-                                description='Standard input for SECIM tools.')
-    standard.add_argument( "-i","--input", dest="input", action='store', required=True, 
-                        help="Input dataset in wide format.")
-    standard.add_argument("-d" ,"--design",dest="design", action='store', required=False,default=False,
-                        help="Design file.")
-    standard.add_argument("-id", "--ID",dest="uniqID", action='store', required=True, 
-                        help="Name of the column with unique identifiers.")
-    standard.add_argument("-x" ,"--X",dest="x", action='store', required=True,
-                        help="Name of column for X values")
-    standard.add_argument("-y", "--Y",dest="y", action='store', required=True, 
-                        help="Name of column for Y values")
-
-    
-
-    output = parser.add_argument_group(title='Required output')
-    output.add_argument("-f","--figure",dest="figure",action="store",required=False,
-                        help="Path of figure.")
-
-    optional = parser.add_argument_group(title='Optional input')
-    optional.add_argument("-g", "--group",dest="group", action='store', required=False, 
-                        default=False,help="Name of the column with groups.")
-    optional.add_argument("-pg","--palette_group",dest="paletteGroup",action='store',
-                        required=False,default="tableau",help="See palettable website")
-    optional.add_argument("-p","--palette",dest="palette",action='store',
-                        required=False,default="Tableau_20",help="See palettable website")
-    standard.add_argument("-pca","--pca",dest="pca",action='store_true',
-                    required=False,default=False,help="PCA ouput data or standard data")
-
+                        formatter_class=RawDescriptionHelpFormatter)
+    # Standard input
+    standard = parser.add_argument_group(title="Standard input", 
+                        description='Standard input for SECIM tools.')
+    standard.add_argument( "-i","--input", dest="input", action='store', 
+                        required=True, help="Input dataset in wide format.")
+    standard.add_argument("-d" ,"--design",dest="design", action='store', 
+                        required=False,default=False, help="Design file.")
+    standard.add_argument("-id", "--ID",dest="uniqID", action='store', 
+                        required=True, help="Name of the column with unique "\
+                        "identifiers.")
+    standard.add_argument("-g", "--group",dest="group", action='store', 
+                        required=False, default=False, help="Name of the column"\
+                        " with groups.")
+    # Tool input
+    tool = parser.add_argument_group(title="Tool especific input")
+    tool.add_argument("-x" ,"--X",dest="x", action='store', 
+                        required=True, help="Name of column for X values")
+    tool.add_argument("-y", "--Y",dest="y", action='store', 
+                        required=True, help="Name of column for Y values")
+    # Tool output
+    output = parser.add_argument_group(title="Output")
+    output.add_argument("-f","--figure",dest="figure",action="store",
+                        required=False, help="Path of figure.")
+    # Plot options
+    plot = parser.add_argument_group(title='Plot options')
+    plot.add_argument("-pal","--palette",dest="palette",action='store',required=False, 
+                        default="tableau", help="Name of the palette to use.")
+    plot.add_argument("-col","--color",dest="color",action="store",required=False, 
+                        default="Tableau_20", help="Name of a valid color scheme"\
+                        " on the selected palette")
     args = parser.parse_args()
+
+    # Standardize paths
+    args.input  = os.path.abspath(args.input)
+    args.figure = os.path.abspath(args.figure)
+    if args.design:
+        args.design = os.path.abspath(args.design)
     return(args)
 
 def main(args):
-    data = args.input
+    # Loading design
     if args.design:
-        design = args.design
-        design = pandas.DataFrame.from_csv(design,sep="\t")
+        design = pd.DataFrame.from_csv(args.design,sep="\t")
         design.reset_index(inplace=True)
     else:
         design = False
-    uniqID = args.uniqID
-    group = args.group
 
-    out = args.figure
+    # Loading wide file
+    wide = pd.DataFrame.from_csv(args.input,sep="\t")
 
-    X = args.x
-    Y = args.y
+    # Create figureHandler object
+    fh = figureHandler(proj="2d",figsize=(14,8))
 
-    paletteGroup = args.paletteGroup
-    palette = args.palette
-
-    usePCA = args.pca
-
-    if usePCA:
-        wide = pandas.DataFrame.from_csv(data,header=3,sep="\t")
-    else:
-        wide = pandas.DataFrame.from_csv(data,sep="\t")
-
-    
-    fh = figureHandler(proj="2d")
-
-
-    if group and uniqID != "rowID":
-        glist=list(design[group])
-        ch = colorHandler(paletteGroup,palette)
-        colorList, ucGroups = ch.getColorsByGroup(design=design,group=group,uGroup=sorted(set(glist)))
+    # If design file with group and the uniqID is "sampleID" then color by group
+    if args.group and args.uniqID == "sampleID":
+        glist=list(design[args.group])
+        colorList, ucGroups = palette.getColorsByGroup(design=design,
+                                group=args.group,uGroup=sorted(set(glist)))
     else:
         glist = list()
-        colorList = list('b') #blue
+        colorList = palette.mpl_colors[0]
         ucGroups = dict()
-        
-    scatter.scatter2D(ax=fh.ax[0],x=list(wide[X]),y=list(wide[Y]),colorList=colorList)
-
-    fh.despine(fh.ax[0])
-    xlim = fh.ax[0].get_xlim()
-    ylim = fh.ax[0].get_ylim()
-    fh.formatAxis(figTitle=X + " vs " + Y, xTitle=X, yTitle=Y, grid=False,xlim=xlim,ylim=ylim)
     
-    if group and uniqID != "rowID":
-        fh.makeLegend(ax=fh.ax[0],ucGroups=ucGroups,group=group)
+    # Plote scatterplot 2D
+    scatter.scatter2D(ax=fh.ax[0], x=list(wide[args.x]), y=list(wide[args.y]),
+                    colorList=colorList)
+
+    # Despine axis (spine = tick)
+    fh.despine(fh.ax[0])
+
+    # Formating axis
+    fh.formatAxis(figTitle=args.x + " vs " + args.y, xTitle=args.x, 
+                yTitle=args.y, grid=False)
+    
+    # If groups are provided create a legend
+    if args.group and args.uniqID == "sampleID":
+        fh.makeLegend(ax=fh.ax[0],ucGroups=ucGroups,group=args.group)
         fh.shrink()
 
-    fh.export(out)
+    # Saving figure to file
+    with PdfPages(args.figure) as pdfOut:
+        fh.addToPdf(dpi=600, pdfPages=pdfOut)
+    logger.info("Script Complete!")
 
 if __name__ == '__main__':
     # Command line options
-    args = getOptions()
+    args   = getOptions()
 
+    # Set logger
+    logger = logging.getLogger()
+    sl.setLogger(logger)
 
+    # Stablishing color palette
+    palette = colorHandler(pal=args.palette, col=args.color)
+    logger.info(u"Using {0} color scheme from {1} palette".format(args.color,
+                args.palette))
+
+    # Run everything
     main(args)
--- scatter_plot_3D.py
+++ scatter_plot_3D.py
@@ -1,124 +1,144 @@
+#!/usr/bin/env python
 ######################################################################################
 # DATE: 2016/August/10
 # 
-# MODULE: scatter3D.py
+# MODULE: scatter_plot_3D.py
 #
-# VERSION: 1.0
+# VERSION: 1.1
 # 
-# AUTHOR: Matt Thoburn (mthoburn@ufl.edu) ed. Miguel Ibarra (miguelib@ufl.edu)
+# AUTHOR: Matt Thoburn (mthoburn@ufl.edu), Miguel Ibarra (miguelib@ufl.edu)
 #
-# DESCRIPTION: This is a standalone executable for graphing 3D scatter plots from wide data
+# DESCRIPTION: This is a standalone executable for graphing 3D scatter plots
+# from wide data
 #
 ################################################################################
-
-# Import Build-In libraries
+# Import built-in libraries
+import os
+import logging
 import argparse
-
-# Import Add-On libraries
-import pandas as pd
-import module_scatter as scatter
-from manager_color import colorHandler
-from manager_figure import figureHandler
 from argparse import RawDescriptionHelpFormatter
 
-# Import Local libraries
-from interface import wideToDesign
+# Import add-on libraries
+import pandas as pd
+from matplotlib.backends.backend_pdf import PdfPages
+
+# Import local data libraries
+from dataManager import logger as sl
+from dataManager.interface import wideToDesign
+
+# Import local plotting libraries
+from visualManager import module_scatter as scatter
+from visualManager.manager_color import colorHandler
+from visualManager.manager_figure import figureHandler
 
 def getOptions(myOpts=None):
-    description=""
+    description="Standa alone function to plot Scatter Plots 3D"
     parser = argparse.ArgumentParser(description=description, 
-                                    formatter_class=RawDescriptionHelpFormatter)
+                        formatter_class=RawDescriptionHelpFormatter)
     standard = parser.add_argument_group(title='Standard input', 
                                 description='Standard input for SECIM tools.')
-    standard.add_argument( "-i","--input", dest="input", action='store', required=True, 
-                        help="Input dataset in wide format.")
-    standard.add_argument("-d" ,"--design",dest="design", action='store', required=False,
-                        default=False,help="Design file.")
-    standard.add_argument("-id", "--ID",dest="uniqID", action='store', required=True, 
-                        help="Name of the column with unique identifiers.")
-    standard.add_argument("-x" ,"--X",dest="x", action='store', required=True,
-                        help="Name of column for X values")
-    standard.add_argument("-y", "--Y",dest="y", action='store', required=True, 
-                        help="Name of column for Y values")
-    standard.add_argument("-z", "--Z",dest="z", action='store', required=True, 
-                        help="Name of column for Z values")
-
-    output = parser.add_argument_group(title='Required output')
-    output.add_argument("-f","--figure",dest="figure",action="store",required=False,
-                        help="Path of figure.")
-
-    optional = parser.add_argument_group(title='Optional input')
-    optional.add_argument("-g", "--group", dest="group", action='store', 
-                        required=False, default=False, help="Name of the column "\
-                        "with groups.")
-    optional.add_argument("-pg", "--palette_group", dest="paletteGroup", action='store',
-                        required=False, default="tableau", help="see palettable"\
-                        " website.")
-    optional.add_argument("-p", "--palette", dest="palette", action='store',
-                        required=False, default="Tableau_20",help="see palettable"\
-                        " website.")
-    optional.add_argument("-r", "--rotation", dest="rotation", action='store',
+    # Standard input
+    standard = parser.add_argument_group(title="Standard input", 
+                        description='Standard input for SECIM tools.')
+    standard.add_argument( "-i","--input", dest="input", action='store', 
+                        required=True, help="Input dataset in wide format.")
+    standard.add_argument("-d" ,"--design",dest="design", action='store', 
+                        required=False,default=False, help="Design file.")
+    standard.add_argument("-id", "--ID",dest="uniqID", action='store', 
+                        required=True, help="Name of the column with unique "\
+                        "identifiers.")
+    standard.add_argument("-g", "--group",dest="group", action='store', 
+                        required=False, default=False, help="Name of the column"\
+                        " with groups.")
+    # Tool input
+    tool = parser.add_argument_group(title="Tool especific input")
+    tool.add_argument("-x" ,"--X",dest="x", action='store', 
+                        required=True, help="Name of column for X values")
+    tool.add_argument("-y", "--Y",dest="y", action='store', 
+                        required=True, help="Name of column for Y values")
+    tool.add_argument("-z", "--Z",dest="z", action='store', 
+                        required=True, help="Name of column for Z values")
+    # Tool output
+    output = parser.add_argument_group(title="Output")
+    output.add_argument("-f","--figure",dest="figure",action="store",
+                        required=False, help="Path of figure.")
+    # Plot options
+    plot = parser.add_argument_group(title='Plot options')
+    plot.add_argument("-pal","--palette",dest="palette",action='store',required=False, 
+                        default="tableau", help="Name of the palette to use.")
+    plot.add_argument("-col","--color",dest="color",action="store",required=False, 
+                        default="Tableau_20", help="Name of a valid color scheme"\
+                        " on the selected palette")
+    plot.add_argument("-r", "--rotation", dest="rotation", action='store',
                         required=False, default=45,help="camera viewing rotation")
-    optional.add_argument("-e", "--elevation", dest="elevation", action='store',
+    plot.add_argument("-e", "--elevation", dest="elevation", action='store',
                         required=False, default=45,help="Camera vieweing elevation")
-    optional.add_argument("-pca", "--pca", dest="pca", action='store_true',
-                        required=False, default=False,help="PCA ouput data or "\
-                        "standard data")
-
     args = parser.parse_args()
+    # Standardize paths
+    args.input  = os.path.abspath(args.input)
+    args.figure = os.path.abspath(args.figure)
+    if args.design:
+        args.design = os.path.abspath(args.design)
     return(args)
      
 def main(args):
-    # Get design
+    # Loading design
     if args.design:
         design = pd.DataFrame.from_csv(args.design,sep="\t")
         design.reset_index(inplace=True)
     else:
         design = False
 
-    # Assigning x,y,z values to easy acces
-    X = args.x
-    Y = args.y
-    Z = args.z
-
-    # Select wether you want to use output from PCA or just a Wide dataset
-    if args.pca:
-        wide = pd.DataFrame.from_csv(args.input,header=3,sep="\t")
-    else:
-        wide = pd.DataFrame.from_csv(args.input,sep="\t")
+    # Loading wide file
+    wide = pd.DataFrame.from_csv(args.input,sep="\t")
 
     # Open Figure handler
-    fh = figureHandler(proj="3d")
+    fh = figureHandler(proj="3d", figsize=(14,8))
 
-    # Get colors
-    if args.group and args.uniqID:
+    # If design file with group and the uniqID is "sampleID" then color by group
+    if args.group and args.uniqID == "sampleID":
         glist=list(design[args.group])
-        ch = colorHandler(args.paletteGroup,args.palette)
-        colorList, ucGroups = ch.getColorsByGroup(design=design,group=args.group,
-                                                    uGroup=sorted(set(glist)))
+        colorList, ucGroups = palette.getColorsByGroup(design=design,
+                                group=args.group,uGroup=sorted(set(glist)))
     else:
         glist = list()
-        colorList = list('b') #blue
+        colorList = palette.mpl_colors[0]
         ucGroups = dict()
     
-    # Plot scatterplot
-    scatter.scatter3D(ax=fh.ax[0],x=list(wide[X]),y=list(wide[Y]),z=list(wide[Z]),
-                    colorList=colorList)
+    # Plot scatterplot 3D
+    scatter.scatter3D(ax=fh.ax[0], x=list(wide[args.x]), y=list(wide[args.y]),
+                    z=list(wide[args.z]), colorList=colorList)
+    
+    # Despine axis (spine = tick)
+    fh.despine(fh.ax[0])
 
     # Give format to the plot
-    fh.format3D(title=X + " vs " + Y + " vs " + Z, xTitle=X, yTitle=Y, zTitle=Z, 
-                rotation=float(args.rotation),elevation=float(args.elevation))
+    fh.format3D(title=args.x + " vs " + args.y + " vs " + args.z, xTitle=args.x,
+                yTitle=args.y, zTitle=args.z, rotation=float(args.rotation),
+                elevation=float(args.elevation))
     
-    # Make legend
-    if args.group and args.uniqID:
-        fh.makeLegend(ax=fh.ax[0], ucGroups=ucGroups, group=args.group)
+    # If groups are provided create a legend
+    if args.group and args.uniqID == "sampleID":
+        fh.makeLegend(ax=fh.ax[0],ucGroups=ucGroups,group=args.group)
         fh.shrink()
 
-    # Export figure
-    fh.export(args.figure)
+    # Saving figure to file
+    with PdfPages(args.figure) as pdfOut:
+        fh.addToPdf(dpi=600, pdfPages=pdfOut)
+    logger.info("Script Complete!")
 
 if __name__ == '__main__':
     # Command line options
-    args = getOptions()
+    args   = getOptions()
 
+    # Set logger
+    logger = logging.getLogger()
+    sl.setLogger(logger)
+
+    # Stablishing color palette
+    palette = colorHandler(pal=args.palette, col=args.color)
+    logger.info(u"Using {0} color scheme from {1} palette".format(args.color,
+                args.palette))
+
+    # Run everything
     main(args)
