c7a82f7fde9fb2740ffc8cc0f02924f239cb5b21
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-01-21 13:48:12 -0500

    Reformat anovaModules/preProcessing.py

--- src/secimtools/anovaModules/preProcessing.py
+++ src/secimtools/anovaModules/preProcessing.py
@@ -1,5 +1,5 @@
-# Import Add-On libraries
 import pandas as pd
+
 
 def preProcessing(factorNames, factorTypes, design):
     """
@@ -37,59 +37,56 @@ def preProcessing(factorNames, factorTypes, design):
     # Split by ',' the names and the types of the factors
     factorNames = factorNames.split(",")
     factorTypes = factorTypes.split(",")
-    
+
     # Get the list of columns on the design file
     designCols = design.columns.tolist()
-    
+
     # Check len of the factors
     # I the number of elements on names foesnt match with
     # the types will rise an error
     if len(factorNames) != len(factorTypes):
         logger.error("Length of Factors doesnt match FactorType")
-        
-    #iterating over factor and factorType
+
+    # iterating over factor and factorType
     categorical = list()
-    numerical   = list()
-    
-    # Identify wheter a factor is categorical or numerical
-    for fName,fType in list(zip(factorNames,factorTypes)):
+    numerical = list()
+
+    # Identify whether a factor is categorical or numerical
+    for fName, fType in list(zip(factorNames, factorTypes)):
         if fName not in designCols:
             logger.error("'{}' is not located in your design file".format(factor))
-        if fType =="C" or fType=="c":
+        if fType == "C" or fType == "c":
             categorical.append(fName)
-        elif fType =="N" or fType=="n":
+        elif fType == "N" or fType == "n":
             numerical.append(fName)
         else:
-            logger.error("'{0}' is not a Valid Flag, use a valid flag to specify" \
-                    "Categorical(C|c) or Numerical (N|n).".format(fType))
+            logger.error(
+                "'{0}' is not a Valid Flag, use a valid flag to specify"
+                "Categorical(C|c) or Numerical (N|n).".format(fType)
+            )
 
     # If nan found then replace it with 0
-    design.fillna("___",inplace=True)
+    design.fillna("___", inplace=True)
 
     # If categorical treat values as str if numerical as float
     for cat in categorical:
-        design[cat]=design[cat].apply(str)
-            
+        design[cat] = design[cat].apply(str)
+
     # Get list of unique levels
-    lvlsNams=[[sorted(list(set(design[category].tolist()))),category] for category \
-            in categorical]
-    
+    lvlsNams = [
+        [sorted(list(set(design[category].tolist()))), category]
+        for category in categorical
+    ]
+
     # Sort levels by number of elements on levels
-    lvlsNams = sorted(lvlsNams,key=lambda x:len(x[0]),reverse=True)
-    lvlsNams =  list(zip(*lvlsNams))
-    
+    lvlsNams = sorted(lvlsNams, key=lambda x: len(x[0]), reverse=True)
+    lvlsNams = list(zip(*lvlsNams))
+
     # Geting sorted levels and names
     levels = list(lvlsNams[0])
-    categorical  = list(lvlsNams[1])
-    
-    # Once sorted create preformula
-    preFormula = ["C({0})".format(cat) for  cat in categorical]
+    categorical = list(lvlsNams[1])
 
-    # Adding numerical at the end
-    preFormula+= numerical
-    
-    # Creating preFormula
-    preFormula =  "+".join(preFormula)
-          
-    # Returning 
-    return preFormula,categorical,numerical,levels,design
+    preFormula = ["C({0})".format(cat) for cat in categorical]
+    preFormula += numerical
+    preFormula = "+".join(preFormula)
+    return preFormula, categorical, numerical, levels, design
