c150fd9e04e2f08b55e7ef945b1672d2fe4c28be
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-01-21 13:54:15 -0500

    Reformat visualManager/module_distribution.py

--- src/secimtools/visualManager/module_distribution.py
+++ src/secimtools/visualManager/module_distribution.py
@@ -1,19 +1,17 @@
 ########################################################################
-# Date: 2016/July/11
 #
 # Module: module_distribution.py
-# 
-# Version: 0.9
 #
-# Author: Matt Thoburn (mthoburn@ufl.edu)
+# Author: Matt Thoburn <mthoburn@ufl.edu>
 #
-# Description: This module contains a method to plot a 
+# Description: This module contains a method to plot a
 #                  kernel density estimate function
 ########################################################################
+
 import pandas as pd
 
 
-def plotDensityDF(data,ax,colors,lb=""):
+def plotDensityDF(data, ax, colors, lb=""):
     """
     Plots a density plot from a dataframe
 
@@ -25,16 +23,14 @@ def plotDensityDF(data,ax,colors,lb=""):
         :param ax: axis to be drawn on
 
         :type colors: list of colors
-        :param colors: colors to be used in differentiating plots 
+        :param colors: colors to be used in differentiating plots
 
         :type lb: string
         :param lb: optional label for legend
     """
-    #Plot density plot
-    data.plot(kind="density",ax=ax,legend=False,color=colors,label=lb,grid=False)
+    data.plot(kind="density", ax=ax, legend=False, color=colors, label=lb, grid=False)
 
-    #Adjust xlim if not negative values
+    # Adjust xlim if values are not negative
     xmin, xmax = ax.get_xlim()
     if xmin <= 0:
         ax.set_xlim(0, xmax)
- 
