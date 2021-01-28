69f82d6fedf1db1e0435f8071afc20d9dcb32a56
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-01-28 13:35:31 -0500

    Add bioconductor-impute to conda requirements, red/green pass for imputation

--- tests/imputation.sh
+++ tests/imputation.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-SCRIPT=$(basename "${BASH_SOURCE[0]}"); NAME="${SCRIPT%.*}"
-TIMESTAMP=$(date +%s)
-SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
-INPUT_DIR="${SCRIPT_DIR}/../test-data"
-OUTPUT_DIR="$(pwd)/output/${NAME}_${TIMESTAMP}"
+#     <test>
+#        <param name="input"      value="ST000006_data.tsv"/>
+#        <param name="design"     value="ST000006_design.tsv"/>
+#        <param name="uniqID"     value="Retention_Index" />
+#        <param name="group"      value="White_wine_type_and_source" />
+#        <param name="imputation" value="knn" />
+#        <output name="imputed"   file="ST000006_imputation_output_KNN.tsv" />
+#     </test>
+
+SCRIPT=$(basename "${BASH_SOURCE[0]}");
+TEST="${SCRIPT%.*}"
+TESTDIR="testout/${TEST}"
+INPUT_DIR="galaxy/test-data"
+OUTPUT_DIR=$TESTDIR
+rm -rf "${TESTDIR}"
+mkdir -p "${TESTDIR}"
+echo "### Starting test: ${TEST}"
 if [[ $# -gt 0 ]]; then OUTPUT_DIR=$1 ; fi
 mkdir -p "${OUTPUT_DIR}"
 
@@ -15,17 +27,4 @@ imputation.py \
     -s knn \
     -o  "$OUTPUT_DIR/ST000006_imputation_output_KNN.tsv"
 
-: '
- The piece of code below is the test for the xml file.
-
-    <tests>
-     <test>
-        <param name="input"      value="ST000006_data.tsv"/>
-        <param name="design"     value="ST000006_design.tsv"/>
-        <param name="uniqID"     value="Retention_Index" />
-        <param name="group"      value="White_wine_type_and_source" />
-        <param name="imputation" value="knn" />
-        <output name="imputed"   file="ST000006_imputation_output_KNN.tsv" />
-     </test>
-    </tests>
-'
+echo "### Finished test: ${TEST} on $(date)"
