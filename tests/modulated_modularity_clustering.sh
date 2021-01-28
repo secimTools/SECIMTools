a34e9f7f36681a47cc18d27e9daaff2d0c49883c
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-01-28 15:34:16 -0500

    Update modulated_modularity_clustering test script to pass red/green test

--- tests/modulated_modularity_clustering.sh
+++ tests/modulated_modularity_clustering.sh
@@ -1,42 +1,30 @@
 #!/bin/bash
-TIMESTAMP=$(date +%s)
-SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+#     <test>
+#        <param name="input"   value="ST000006_data.tsv"/>
+#        <param name="design"  value="ST000006_design.tsv"/>
+#        <param name="uniqID"  value="Retention_Index" />
+#        <param name="corr"    value="pearson" />
+#        <output name="output" file="ST000006_modulated_modularity_clustering_out.tsv" compare="sim_size" delta="10000"/>
+#        <output name="figure" file="ST000006_modulated_modularity_clustering_figure.pdf" compare="sim_size" delta="10000" />
+#     </test>
 
-CODEDIR="${SCRIPT_DIR}/.."
-INPUTDIR="${SCRIPT_DIR}/../test-data"
-OUTPUTDIR="$(pwd)/output_${TIMESTAMP}"
-mkdir -p ${OUTPUTDIR}
+SCRIPT=$(basename "${BASH_SOURCE[0]}");
+TEST="${SCRIPT%.*}"
+TESTDIR="testout/${TEST}"
+INPUT_DIR="galaxy/test-data"
+OUTPUT_DIR=$TESTDIR
+rm -rf "${TESTDIR}"
+mkdir -p "${TESTDIR}"
+echo "### Starting test: ${TEST}"
+if [[ $# -gt 0 ]]; then OUTPUT_DIR=$1 ; fi
+mkdir -p "${OUTPUT_DIR}"
 
-/modulated_modularity_clustering.py \
-		-i  $INPUTDIR/ST000006_data.tsv \
-		-d  $INPUTDIR/ST000006_design.tsv \
-		-id Retention_Index \
-                -c pearson \
-         	-o $OUTPUTDIR/ST000006_modulated_modularity_clustering_out.tsv \
-	        -f $OUTPUTDIR/ST000006_modulated_modularity_clustering_figure.pdf 
+modulated_modularity_clustering.py \
+    -i  "$INPUT_DIR/ST000006_data.tsv" \
+    -d  "$INPUT_DIR/ST000006_design.tsv" \
+    -id Retention_Index \
+    -c pearson \
+    -o "$OUTPUT_DIR/ST000006_modulated_modularity_clustering_out.tsv" \
+    -f "$OUTPUT_DIR/ST000006_modulated_modularity_clustering_figure.pdf"
 
-
-
-
-
-
-
-: '
-
-The piece of code below is the test for the xml file.
-
-    <tests>
-     <test>
-        <param name="input"   value="ST000006_data.tsv"/>
-        <param name="design"  value="ST000006_design.tsv"/>
-        <param name="uniqID"  value="Retention_Index" />
-        <param name="corr"    value="pearson" />
-        <output name="output" file="ST000006_modulated_modularity_clustering_out.tsv" compare="sim_size" delta="10000"/>
-        <output name="figure" file="ST000006_modulated_modularity_clustering_figure.pdf" compare="sim_size" delta="10000" />
-     </test>
-    </tests>
-
-'
-
-
-
+echo "### Finished test: ${TEST} on $(date)"
