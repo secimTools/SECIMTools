327d226a46393186e527fbe859219ac308c44135
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-01-28 13:10:03 -0500

    Red/green test pass for bland_altman_plot_with_group

--- tests/bland_altman_plot_with_group.sh
+++ tests/bland_altman_plot_with_group.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-SCRIPT=$(basename "${BASH_SOURCE[0]}"); NAME="${SCRIPT%.*}"
-TIMESTAMP=$(date +%s)
-SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
-INPUT_DIR="${SCRIPT_DIR}/../test-data"
-OUTPUT_DIR="$(pwd)/output/${NAME}_${TIMESTAMP}"
+#     <test>
+#        <param name="input"  value="ST000006_data.tsv"/>
+#        <param name="design" value="ST000006_design.tsv"/>
+#        <param name="uniqID" value="Retention_Index" />
+#        <param name="group"  value="White_wine_type_and_source" />
+#        <output name="ba_plots"           file="ST000006_bland_altman_plot_with_group_figure.pdf" compare="sim_size" delta="10000" />
+#        <output name="outlier_dist_plots" file="ST000006_bland_altman_plot_with_group_flag_distribution.pdf" compare="sim_size" delta="10000" />
+#        <output name="flag_sample"        file="ST000006_bland_altman_plot_with_group_flag_sample.tsv" />
+#        <output name="flag_feature"       file="ST000006_bland_altman_plot_with_group_flag_feature.tsv" />
+#     </test>
+
+TEST="bland_altman_plot_with_group"
+TESTDIR="testout/${TEST}"
+rm -rf ${TESTDIR}
+mkdir -p ${TESTDIR}
+echo "### Starting test: ${TEST}"
+
+INPUT_DIR="galaxy/test-data"
+OUTPUT_DIR=$TESTDIR
 if [[ $# -gt 0 ]]; then OUTPUT_DIR=$1 ; fi
 mkdir -p "${OUTPUT_DIR}"
 
@@ -19,21 +33,5 @@ bland_altman_plot.py \
     -pf "$OUTPUT_DIR/ST000006_bland_altman_plot_prop_feature.tsv" \
     -ps "$OUTPUT_DIR/ST000006_bland_altman_plot_prop_sample.tsv"
 
-: '
-
- The piece of code below is the test for the xml file.
-
-    <tests>
-     <test>
-        <param name="input"  value="ST000006_data.tsv"/>
-        <param name="design" value="ST000006_design.tsv"/>
-        <param name="uniqID" value="Retention_Index" />
-        <param name="group"  value="White_wine_type_and_source" />
-        <output name="ba_plots"           file="ST000006_bland_altman_plot_with_group_figure.pdf" compare="sim_size" delta="10000" />
-        <output name="outlier_dist_plots" file="ST000006_bland_altman_plot_with_group_flag_distribution.pdf" compare="sim_size" delta="10000" />
-        <output name="flag_sample"        file="ST000006_bland_altman_plot_with_group_flag_sample.tsv" />
-        <output name="flag_feature"       file="ST000006_bland_altman_plot_with_group_flag_feature.tsv" />
-     </test>
-    </tests>
-
-'
+    date
+echo "### Finished test: ${TEST}"
