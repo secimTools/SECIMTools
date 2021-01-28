02f6bf7b5c407e9b4f03d486e236475da5842420
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-01-28 15:34:33 -0500

    Update multiple_testing_adjustment test script to pass red/green test

--- tests/multiple_testing_adjustment.sh
+++ tests/multiple_testing_adjustment.sh
@@ -1,35 +1,30 @@
 #!/bin/bash
-TIMESTAMP=$(date +%s)
-SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+#     <test>
+#        <param name="input"  value="ST000006_anova_fixed_with_group_summary.tsv"/>
+#        <param name="uniqID" value="Retention_Index" />
+#        <param name="pval"   value="prob_greater_than_t_for_diff_Chardonnay, Carneros, CA 2003 (CH01)-Chardonnay, Carneros, CA 2003 (CH02)" />
+#        <param name="alpha"  value="0.05" />
+#        <output name="outadjusted" file="ST000006_multiple_testing_adjustment_outadjusted.tsv" />
+#        <output name="flags"       file="ST000006_multiple_testing_adjustment_flags.tsv" />
+#     </test>
 
-CODEDIR="${SCRIPT_DIR}/.."
-INPUTDIR="${SCRIPT_DIR}/../test-data"
-OUTPUTDIR="$(pwd)/output_${TIMESTAMP}"
-mkdir -p ${OUTPUTDIR}
+SCRIPT=$(basename "${BASH_SOURCE[0]}");
+TEST="${SCRIPT%.*}"
+TESTDIR="testout/${TEST}"
+INPUT_DIR="galaxy/test-data"
+OUTPUT_DIR=$TESTDIR
+rm -rf "${TESTDIR}"
+mkdir -p "${TESTDIR}"
+echo "### Starting test: ${TEST}"
+if [[ $# -gt 0 ]]; then OUTPUT_DIR=$1 ; fi
+mkdir -p "${OUTPUT_DIR}"
 
-/multiple_testing_adjustment.py \
-		-i  $INPUTDIR/ST000006_anova_fixed_with_group_summary.tsv \
-		-id Retention_Index \
-                -pv  "prob_greater_than_t_for_diff_Chardonnay, Carneros, CA 2003 (CH01)-Chardonnay, Carneros, CA 2003 (CH02)" \
-		-a 0.05 \
-         	-on $OUTPUTDIR/ST000006_multiple_testing_adjustment_outadjusted.tsv \
-         	-fl $OUTPUTDIR/ST000006_multiple_testing_adjustment_flags.tsv 
+multiple_testing_adjustment.py \
+    -i  "$INPUT_DIR/ST000006_anova_fixed_with_group_summary.tsv" \
+    -id Retention_Index \
+    -pv "prob_greater_than_t_for_diff_Chardonnay, Carneros, CA 2003 (CH01)-Chardonnay, Carneros, CA 2003 (CH02)" \
+    -a  0.05 \
+    -on "$OUTPUT_DIR/ST000006_multiple_testing_adjustment_outadjusted.tsv" \
+    -fl "$OUTPUT_DIR/ST000006_multiple_testing_adjustment_flags.tsv"
 
-
-: '
-
- The piece of code below is the test for the xml file.
-
-    <tests>
-     <test>
-        <param name="input"  value="ST000006_anova_fixed_with_group_summary.tsv"/>
-        <param name="uniqID" value="Retention_Index" />
-        <param name="pval"   value="prob_greater_than_t_for_diff_Chardonnay, Carneros, CA 2003 (CH01)-Chardonnay, Carneros, CA 2003 (CH02)" />
-        <param name="alpha"  value="0.05" />
-        <output name="outadjusted" file="ST000006_multiple_testing_adjustment_outadjusted.tsv" />
-        <output name="flags"       file="ST000006_multiple_testing_adjustment_flags.tsv" />
-     </test>
-    </tests>
-
-'
-
+echo "### Finished test: ${TEST} on $(date)"
