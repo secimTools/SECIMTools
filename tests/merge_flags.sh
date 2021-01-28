18a08ce5ac94f0f6647b0d7b6eaf036f655757f6
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-01-28 15:33:36 -0500

    Update merge_flags test script to pass red/green test

--- tests/merge_flags.sh
+++ tests/merge_flags.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
-TIMESTAMP=$(date +%s)
-SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+#     <test>
+#        <param name="input"      value="ST000006_run_order_regression_flags.tsv,ST000006_lasso_enet_var_select_flags.tsv"/>
+#        <param name="flagUniqID" value="Retention_Index" />
+#        <param name="filename"   value="ST000006_run_order_regression_flags ST000006_lasso_enet_var_select_flags" />
+#        <output name="output"    file="ST000006_merge_flags_output.tsv" />
+#     </test>
 
-CODEDIR="${SCRIPT_DIR}/.."
-INPUTDIR="${SCRIPT_DIR}/../test-data"
-OUTPUTDIR="$(pwd)/output_${TIMESTAMP}"
-mkdir -p ${OUTPUTDIR}
+SCRIPT=$(basename "${BASH_SOURCE[0]}");
+TEST="${SCRIPT%.*}"
+TESTDIR="testout/${TEST}"
+INPUT_DIR="galaxy/test-data"
+OUTPUT_DIR=$TESTDIR
+rm -rf "${TESTDIR}"
+mkdir -p "${TESTDIR}"
+echo "### Starting test: ${TEST}"
+if [[ $# -gt 0 ]]; then OUTPUT_DIR=$1 ; fi
+mkdir -p "${OUTPUT_DIR}"
 
-/merge_flags.py \
-		-i  "$INPUTDIR/ST000006_run_order_regression_flags.tsv,$INPUTDIR/ST000006_lasso_enet_var_select_flags.tsv" \
-		-f  "ST000006_run_order_regression_flags_tsv" "ST000006_lasso_enet_var_select_flags_tsv" \
-		-fid Retention_Index \
-         	-o  $OUTPUTDIR/ST000006_merge_flags_output.tsv \
+merge_flags.py \
+    -i  "$INPUT_DIR/ST000006_run_order_regression_flags.tsv,$INPUT_DIR/ST000006_lasso_enet_var_select_flags.tsv" \
+    -f  "ST000006_run_order_regression_flags_tsv" "ST000006_lasso_enet_var_select_flags_tsv" \
+    -fid Retention_Index \
+    -o  "$OUTPUT_DIR/ST000006_merge_flags_output.tsv" \
 
-
-: '
-
-The piece of code below is the test for the xml file.
-
-    <tests>
-     <test>
-        <param name="input"      value="ST000006_run_order_regression_flags.tsv,ST000006_lasso_enet_var_select_flags.tsv"/>
-        <param name="flagUniqID" value="Retention_Index" />
-        <param name="filename"   value="ST000006_run_order_regression_flags ST000006_lasso_enet_var_select_flags" />
-        <output name="output"    file="ST000006_merge_flags_output.tsv" />
-     </test>
-    </tests>
-
-'
-
+echo "### Finished test: ${TEST} on $(date)"
