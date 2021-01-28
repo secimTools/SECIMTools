43326c3579687f343e8542086413a3484cdc320f
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-01-28 13:01:16 -0500

    Ignore testout directory tree, red/green pass for anova_fixed_with_group

--- tests/anova_fixed_with_group.sh
+++ tests/anova_fixed_with_group.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-SCRIPT=$(basename "${BASH_SOURCE[0]}"); NAME="${SCRIPT%.*}"
-TIMESTAMP=$(date +%s)
-SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
-INPUT_DIR="${SCRIPT_DIR}/../test-data"
-OUTPUT_DIR="$(pwd)/output/${NAME}_${TIMESTAMP}"
+#     <test>
+#        <param name="input"       value="ST000006_data.tsv"/>
+#        <param name="design"      value="ST000006_design.tsv"/>
+#        <param name="uniqID"      value="Retention_Index" />
+#        <param name="factor"      value="White_wine_type_and_source" />
+#        <param name="factorTypes" value="C" />
+#        <output name="results_table" file="ST000006_anova_fixed_with_group_summary.tsv" />
+#        <output name="flags_table"   file="ST000006_anova_fixed_with_group_flags.tsv" />
+#        <output name="qq_plots"      file="ST000006_anova_fixed_with_group_qq_plots.pdf" compare="sim_size" delta="10000" />
+#        <output name="volcano_plots" file="ST000006_anova_fixed_with_group_volcano.pdf"  compare="sim_size" delta="10000" />
+#     </test>
+
+TEST="anova_fixed_with_group"
+TESTDIR="testout/${TEST}"
+rm -rf ${TESTDIR}
+mkdir -p ${TESTDIR}
+echo "### Starting test: ${TEST}"
+
+INPUT_DIR="galaxy/test-data"
+OUTPUT_DIR=$TESTDIR
 if [[ $# -gt 0 ]]; then OUTPUT_DIR=$1 ; fi
 mkdir -p "${OUTPUT_DIR}"
 
+# Symlink into the conda bin directory
 anova_fixed.py \
     -i  "$INPUT_DIR/ST000006_data.tsv" \
     -d  "$INPUT_DIR/ST000006_design.tsv" \
@@ -18,21 +34,5 @@ anova_fixed.py \
     -f1 "$OUTPUT_DIR/ST000006_anova_fixed_with_group_qq_plots.pdf" \
     -f2 "$OUTPUT_DIR/ST000006_anova_fixed_with_group_volcano.pdf"
 
-: '
-The piece of code below is the test for the xml file.
-
-    <tests>
-     <test>
-        <param name="input"       value="ST000006_data.tsv"/>
-        <param name="design"      value="ST000006_design.tsv"/>
-        <param name="uniqID"      value="Retention_Index" />
-        <param name="factor"      value="White_wine_type_and_source" />
-        <param name="factorTypes" value="C" />
-        <output name="results_table" file="ST000006_anova_fixed_with_group_summary.tsv" />
-        <output name="flags_table"   file="ST000006_anova_fixed_with_group_flags.tsv" />
-        <output name="qq_plots"      file="ST000006_anova_fixed_with_group_qq_plots.pdf" compare="sim_size" delta="10000" />
-        <output name="volcano_plots" file="ST000006_anova_fixed_with_group_volcano.pdf"  compare="sim_size" delta="10000" />
-     </test>
-    </tests>
-
-'
+date
+echo "### Finished test: ${TEST}"
