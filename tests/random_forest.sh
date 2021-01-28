ef1c88f45c68390c0ee282d7bd391d9ec903b007
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-01-28 15:36:14 -0500

    Update random_forest test script for red/gree testing. TODO fix failing code

--- tests/random_forest.sh
+++ tests/random_forest.sh
@@ -1,37 +1,32 @@
 #!/bin/bash
-TIMESTAMP=$(date +%s)
-SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+#     <test>
+#        <param name="input"  value="ST000006_data.tsv"/>
+#        <param name="design" value="ST000006_design.tsv"/>
+#        <param name="uniqID" value="Retention_Index" />
+#        <param name="group"  value="White_wine_type_and_source" />
+#        <output name="outfile1" file="ST000006_random_forest_out.tsv" />
+#        <output name="outfile2" file="ST000006_random_forest_out2.tsv" />
+#        <output name="figure"   file="ST000006_random_forest_figure.pdf" compare="sim_size" delta="10000"/>
+#     </test>
 
-CODEDIR="${SCRIPT_DIR}/.."
-INPUTDIR="${SCRIPT_DIR}/../test-data"
-OUTPUTDIR="$(pwd)/output_${TIMESTAMP}"
-mkdir -p ${OUTPUTDIR}
+SCRIPT=$(basename "${BASH_SOURCE[0]}");
+TEST="${SCRIPT%.*}"
+TESTDIR="testout/${TEST}"
+INPUT_DIR="galaxy/test-data"
+OUTPUT_DIR=$TESTDIR
+rm -rf "${TESTDIR}"
+mkdir -p "${TESTDIR}"
+echo "### Starting test: ${TEST}"
+if [[ $# -gt 0 ]]; then OUTPUT_DIR=$1 ; fi
+mkdir -p "${OUTPUT_DIR}"
 
-/random_forest.py \
-		-i  $INPUTDIR/ST000006_data.tsv \
-		-d  $INPUTDIR/ST000006_design.tsv \
-		-id Retention_Index \
-                -g White_wine_type_and_source \
-         	-o  $OUTPUTDIR/ST000006_random_forest_out.tsv \
-	        -o2 $OUTPUTDIR/ST000006_random_forest_out2.tsv \
-         	-f  $OUTPUTDIR/ST000006_random_forest_figure.pdf
+random_forest.py \
+    -i  "$INPUT_DIR/ST000006_data.tsv" \
+    -d  "$INPUT_DIR/ST000006_design.tsv" \
+    -id Retention_Index \
+    -g White_wine_type_and_source \
+    -o  "$OUTPUT_DIR/ST000006_random_forest_out.tsv" \
+    -o2 "$OUTPUT_DIR/ST000006_random_forest_out2.tsv" \
+    -f  "$OUTPUT_DIR/ST000006_random_forest_figure.pdf"
 
-
-: '
-
-The piece of code below is the test for the xml file.
-
-    <tests>
-     <test>
-        <param name="input"  value="ST000006_data.tsv"/>
-        <param name="design" value="ST000006_design.tsv"/>
-        <param name="uniqID" value="Retention_Index" />
-        <param name="group"  value="White_wine_type_and_source" />
-        <output name="outfile1" file="ST000006_random_forest_out.tsv" />
-        <output name="outfile2" file="ST000006_random_forest_out2.tsv" />
-        <output name="figure"   file="ST000006_random_forest_figure.pdf" compare="sim_size" delta="10000"/>
-     </test>
-    </tests>
-
-'
-
+echo "### Finished test: ${TEST} on $(date)"
