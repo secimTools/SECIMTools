4d8a962a4776ed1cfa1cd8f8fcb99b29cf573708
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-01-28 15:33:08 -0500

    Update mahalanobis_distance test script for red/gree testing. TODO fix failing code

--- tests/mahalanobis_distance.sh
+++ tests/mahalanobis_distance.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
-TIMESTAMP=$(date +%s)
-SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+#     <test>
+#        <param name="input"   value="ST000006_data.tsv"/>
+#        <param name="design"  value="ST000006_design.tsv"/>
+#        <param name="uniqID"  value="Retention_Index" />
+#        <param name="group"   value="White_wine_type_and_source" />
+#        <param name="penalty" value="0.5" />
+#        <output name="plot"   file="ST000006_mahalanobis_distance_figure.pdf" compare="sim_size" delta="10000" />
+#        <output name="out1"   file="ST000006_mahalanobis_distance_to_mean.tsv" />
+#        <output name="out2"   file="ST000006_mahalanobis_distance_pairwise.tsv" />
+#     </test>
 
-CODEDIR="${SCRIPT_DIR}/.."
-INPUTDIR="${SCRIPT_DIR}/../test-data"
-OUTPUTDIR="$(pwd)/output_${TIMESTAMP}"
-mkdir -p ${OUTPUTDIR}
+SCRIPT=$(basename "${BASH_SOURCE[0]}");
+TEST="${SCRIPT%.*}"
+TESTDIR="testout/${TEST}"
+INPUT_DIR="galaxy/test-data"
+OUTPUT_DIR=$TESTDIR
+rm -rf "${TESTDIR}"
+mkdir -p "${TESTDIR}"
+echo "### Starting test: ${TEST}"
+if [[ $# -gt 0 ]]; then OUTPUT_DIR=$1 ; fi
+mkdir -p "${OUTPUT_DIR}"
 
-/mahalanobis_distance.py \
-		-i  $INPUTDIR/ST000006_data.tsv \
-		-d  $INPUTDIR/ST000006_design.tsv \
-		-id Retention_Index \
-                -g  White_wine_type_and_source \
-		-pen 0.5 \
-         	-m  $OUTPUTDIR/ST000006_mahalanobis_distance_to_mean.tsv \
-         	-f  $OUTPUTDIR/ST000006_mahalanobis_distance_figure.pdf \
-         	-pw $OUTPUTDIR/ST000006_mahalanobis_distance_pairwise.tsv   
+mahalanobis_distance.py \
+   -i  "$INPUT_DIR/ST000006_data.tsv" \
+   -d  "$INPUT_DIR/ST000006_design.tsv" \
+   -id Retention_Index \
+   -g  White_wine_type_and_source \
+   -pen 0.5 \
+   -m  "$OUTPUT_DIR/ST000006_mahalanobis_distance_to_mean.tsv" \
+   -f  "$OUTPUT_DIR/ST000006_mahalanobis_distance_figure.pdf" \
+   -pw "$OUTPUT_DIR/ST000006_mahalanobis_distance_pairwise.tsv"
 
-
-: '
-
- The piece of code below is the test for the xml file.
-
-    <tests>
-     <test>
-        <param name="input"   value="ST000006_data.tsv"/>
-        <param name="design"  value="ST000006_design.tsv"/>
-        <param name="uniqID"  value="Retention_Index" />
-        <param name="group"   value="White_wine_type_and_source" />
-        <param name="penalty" value="0.5" />
-        <output name="plot"   file="ST000006_mahalanobis_distance_figure.pdf" compare="sim_size" delta="10000" />
-        <output name="out1"   file="ST000006_mahalanobis_distance_to_mean.tsv" />
-        <output name="out2"   file="ST000006_mahalanobis_distance_pairwise.tsv" />
-     </test>
-    </tests>
-
-'
-
+echo "### Finished test: ${TEST} on $(date)"
