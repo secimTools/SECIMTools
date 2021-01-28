3e47e56ca4df457ad66d829fa4f7e34a489b8260
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-01-28 15:30:09 -0500

    Update kruskal_wallis_with_group test script for red/gree testing. TODO fix failing code

--- tests/kruskal_wallis_with_group.sh
+++ tests/kruskal_wallis_with_group.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-SCRIPT=$(basename "${BASH_SOURCE[0]}"); NAME="${SCRIPT%.*}"
-TIMESTAMP=$(date +%s)
-SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
-INPUT_DIR="${SCRIPT_DIR}/../test-data"
-OUTPUT_DIR="$(pwd)/output/${NAME}_${TIMESTAMP}"
+#     <test>
+#        <param name="input"    value="ST000006_data.tsv"/>
+#        <param name="design"   value="ST000006_design.tsv"/>
+#        <param name="uniqueID" value="Retention_Index" />
+#        <param name="group"    value="White_wine_type_and_source" />
+#        <output name="summaries" file="ST000006_kruskal_wallis_with_group_summary.tsv" />
+#        <output name="flags"     file="ST000006_kruskal_wallis_with_group_flags.tsv" />
+#        <output name="volcano"   file="ST000006_kruskal_wallis_with_group_volcano.pdf" compare="sim_size" delta="10000" />
+#     </test>
+
+SCRIPT=$(basename "${BASH_SOURCE[0]}");
+TEST="${SCRIPT%.*}"
+TESTDIR="testout/${TEST}"
+INPUT_DIR="galaxy/test-data"
+OUTPUT_DIR=$TESTDIR
+rm -rf "${TESTDIR}"
+mkdir -p "${TESTDIR}"
+echo "### Starting test: ${TEST}"
 if [[ $# -gt 0 ]]; then OUTPUT_DIR=$1 ; fi
 mkdir -p "${OUTPUT_DIR}"
 
@@ -16,18 +29,4 @@ kruskal_wallis.py \
     -f "$OUTPUT_DIR/ST000006_kruskal_wallis_with_group_flags.tsv" \
     -v "$OUTPUT_DIR/ST000006_kruskal_wallis_with_group_volcano.pdf"
 
-: '
- The piece of code below is the test for the xml file.
-
-    <tests>
-     <test>
-        <param name="input"    value="ST000006_data.tsv"/>
-        <param name="design"   value="ST000006_design.tsv"/>
-        <param name="uniqueID" value="Retention_Index" />
-        <param name="group"    value="White_wine_type_and_source" />
-        <output name="summaries" file="ST000006_kruskal_wallis_with_group_summary.tsv" />
-        <output name="flags"     file="ST000006_kruskal_wallis_with_group_flags.tsv" />
-        <output name="volcano"   file="ST000006_kruskal_wallis_with_group_volcano.pdf" compare="sim_size" delta="10000" />
-     </test>
-    </tests>
-'
+echo "### Finished test: ${TEST} on $(date)"
