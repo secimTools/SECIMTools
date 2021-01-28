59d661d02da8c353e057ace449c99b89b11d6d16
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-01-28 15:35:57 -0500

    Update principal_component_analysis test script to pass red/green test

--- tests/principal_component_analysis.sh
+++ tests/principal_component_analysis.sh
@@ -1,40 +1,34 @@
 #!/bin/bash
-TIMESTAMP=$(date +%s)
-SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+#     <test>
+#        <param name="input"  value="ST000006_data.tsv"/>
+#        <param name="design" value="ST000006_design.tsv"/>
+#        <param name="uniqID" value="Retention_Index" />
+#        <param name="group"  value="White_wine_type_and_source" />
+#        <output name="loadings" file="ST000006_principal_component_analysis_load_out.tsv" />
+#        <output name="scores"   file="ST000006_principal_component_analysis_score_out.tsv" />
+#        <output name="summary"  file="ST000006_principal_component_analysis_summary_out.tsv" />
+#        <output name="figures"  file="ST000006_principal_component_analysis_figure.pdf" compare="sim_size" delta="10000"/>
+#     </test>
 
-CODEDIR="${SCRIPT_DIR}/.."
-INPUTDIR="${SCRIPT_DIR}/../test-data"
-OUTPUTDIR="$(pwd)/output_${TIMESTAMP}"
-mkdir -p ${OUTPUTDIR}
+SCRIPT=$(basename "${BASH_SOURCE[0]}");
+TEST="${SCRIPT%.*}"
+TESTDIR="testout/${TEST}"
+INPUT_DIR="galaxy/test-data"
+OUTPUT_DIR=$TESTDIR
+rm -rf "${TESTDIR}"
+mkdir -p "${TESTDIR}"
+echo "### Starting test: ${TEST}"
+if [[ $# -gt 0 ]]; then OUTPUT_DIR=$1 ; fi
+mkdir -p "${OUTPUT_DIR}"
 
-/principal_component_analysis.py \
-		-i  $INPUTDIR/ST000006_data.tsv \
-		-d  $INPUTDIR/ST000006_design.tsv \
-		-id Retention_Index \
-                -g White_wine_type_and_source \
-         	-lo $OUTPUTDIR/ST000006_principal_component_analysis_load_out.tsv \
-	        -so $OUTPUTDIR/ST000006_principal_component_analysis_score_out.tsv \
-         	-su $OUTPUTDIR/ST000006_principal_component_analysis_summary_out.tsv \
-         	-f  $OUTPUTDIR/ST000006_principal_component_analysis_figure.pdf
+principal_component_analysis.py \
+    -i  "$INPUT_DIR/ST000006_data.tsv" \
+    -d  "$INPUT_DIR/ST000006_design.tsv" \
+    -id Retention_Index \
+    -g White_wine_type_and_source \
+    -lo "$OUTPUT_DIR/ST000006_principal_component_analysis_load_out.tsv" \
+    -so "$OUTPUT_DIR/ST000006_principal_component_analysis_score_out.tsv" \
+    -su "$OUTPUT_DIR/ST000006_principal_component_analysis_summary_out.tsv" \
+    -f  "$OUTPUT_DIR/ST000006_principal_component_analysis_figure.pdf"
 
-
-
-: '
-
-The piece of code below is the test for the xml file.
-
-    <tests>
-     <test>
-        <param name="input"  value="ST000006_data.tsv"/>
-        <param name="design" value="ST000006_design.tsv"/>
-        <param name="uniqID" value="Retention_Index" />
-        <param name="group"  value="White_wine_type_and_source" />
-        <output name="loadings" file="ST000006_principal_component_analysis_load_out.tsv" />
-        <output name="scores"   file="ST000006_principal_component_analysis_score_out.tsv" />
-        <output name="summary"  file="ST000006_principal_component_analysis_summary_out.tsv" />
-        <output name="figures"  file="ST000006_principal_component_analysis_figure.pdf" compare="sim_size" delta="10000"/>
-     </test>
-    </tests>
-
-'
-
+echo "### Finished test: ${TEST} on $(date)"
