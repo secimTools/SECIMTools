c083e642840e981b10eef23ca6b38ccda8f338de
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-01-28 15:39:16 -0500

    Update threshold_based_flags test script to pass red/green test

--- tests/threshold_based_flags.sh
+++ tests/threshold_based_flags.sh
@@ -1,35 +1,30 @@
 #!/bin/bash
-TIMESTAMP=$(date +%s)
-SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+#     <test>
+#        <param name="input"   value="ST000006_data.tsv"/>
+#        <param name="design"  value="ST000006_design.tsv"/>
+#        <param name="uniqID"  value="Retention_Index" />
+#        <param name="group"   value="White_wine_type_and_source" />
+#        <param name="cutoff"  value="3000" />
+#        <output name="output" file="ST000006_threshold_based_flags_output.tsv" />
+#     </test>
 
-CODEDIR="${SCRIPT_DIR}/.."
-INPUTDIR="${SCRIPT_DIR}/../test-data"
-OUTPUTDIR="$(pwd)/output_${TIMESTAMP}"
-mkdir -p ${OUTPUTDIR}
+SCRIPT=$(basename "${BASH_SOURCE[0]}");
+TEST="${SCRIPT%.*}"
+TESTDIR="testout/${TEST}"
+INPUT_DIR="galaxy/test-data"
+OUTPUT_DIR=$TESTDIR
+rm -rf "${TESTDIR}"
+mkdir -p "${TESTDIR}"
+echo "### Starting test: ${TEST}"
+if [[ $# -gt 0 ]]; then OUTPUT_DIR=$1 ; fi
+mkdir -p "${OUTPUT_DIR}"
 
-/threshold_based_flags.py \
-		-i  $INPUTDIR/ST000006_data.tsv \
-		-d  $INPUTDIR/ST000006_design.tsv \
-		-id Retention_Index \
-                -g White_wine_type_and_source \
-                -c 3000 \
-         	-o  $OUTPUTDIR/ST000006_threshold_based_flags_output.tsv \
+threshold_based_flags.py \
+    -i  "$INPUT_DIR/ST000006_data.tsv" \
+    -d  "$INPUT_DIR/ST000006_design.tsv" \
+    -id "Retention_Index" \
+    -g  "White_wine_type_and_source" \
+    -c  "3000" \
+    -o  "$OUTPUT_DIR/ST000006_threshold_based_flags_output.tsv"
 
-
-: '
-
-The piece of code below is the test for the xml file.
-
-    <tests>
-     <test>
-        <param name="input"   value="ST000006_data.tsv"/>
-        <param name="design"  value="ST000006_design.tsv"/>
-        <param name="uniqID"  value="Retention_Index" />
-        <param name="group"   value="White_wine_type_and_source" />
-        <param name="cutoff"  value="3000" />
-        <output name="output" file="ST000006_threshold_based_flags_output.tsv" />
-     </test>
-    </tests>
-
-'
-
+echo "### Finished test: ${TEST} on $(date)"
