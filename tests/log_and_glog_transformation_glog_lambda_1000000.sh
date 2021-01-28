2996e968a91f7175a33c83e8128c042a2fba4803
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-01-28 15:31:41 -0500

    Updated log_and_glog_transformation_glog_lambda_1000000 test script to pass red/green test

--- tests/log_and_glog_transformation_glog_lambda_1000000.sh
+++ tests/log_and_glog_transformation_glog_lambda_1000000.sh
@@ -1,41 +1,32 @@
 #!/bin/bash
-TIMESTAMP=$(date +%s)
-SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+#      <test>
+#        <param name="input"          value="ST000006_data.tsv"/>
+#        <param name="design"         value="ST000006_design.tsv"/>
+#        <param name="uniqID"         value="Retention_Index" />
+#        <param name="transformation" value="glog" />
+#        <param name="log_base"       value="log" />
+#        <param name="lambda_value"   value="1000000" />
+#        <output name="oname"         file="ST000006_log_and_glog_transformation_glog_lambda_1000000.tsv" />
+#      </test>
 
-CODEDIR="${SCRIPT_DIR}/.."
-INPUTDIR="${SCRIPT_DIR}/../test-data"
-OUTPUTDIR="$(pwd)/output_${TIMESTAMP}"
-mkdir -p ${OUTPUTDIR}
+SCRIPT=$(basename "${BASH_SOURCE[0]}");
+TEST="${SCRIPT%.*}"
+TESTDIR="testout/${TEST}"
+INPUT_DIR="galaxy/test-data"
+OUTPUT_DIR=$TESTDIR
+rm -rf "${TESTDIR}"
+mkdir -p "${TESTDIR}"
+echo "### Starting test: ${TEST}"
+if [[ $# -gt 0 ]]; then OUTPUT_DIR=$1 ; fi
+mkdir -p "${OUTPUT_DIR}"
 
-/log_and_glog_transformation.py \
-		-i  $INPUTDIR/ST000006_data.tsv \
-		-d  $INPUTDIR/ST000006_design.tsv \
-		-id Retention_Index \
-		-t glog \
-		-l log \
-		-la 1000000 \
-		-o $OUTPUTDIR/ST000006_log_and_glog_transformation_glog_lambda_1000000.tsv 
+log_and_glog_transformation.py \
+        -i  "$INPUT_DIR/ST000006_data.tsv" \
+        -d  "$INPUT_DIR/ST000006_design.tsv" \
+        -id Retention_Index \
+        -t glog \
+        -l log \
+        -la 1000000 \
+        -o "$OUTPUT_DIR/ST000006_log_and_glog_transformation_glog_lambda_1000000.tsv"
 
-
-
-
-: '
- The piece of code below is the test for the xml file.
-
-    <tests>
-      <test>
-        <param name="input"          value="ST000006_data.tsv"/>
-        <param name="design"         value="ST000006_design.tsv"/>
-        <param name="uniqID"         value="Retention_Index" />
-        <param name="transformation" value="glog" />
-        <param name="log_base"       value="log" />
-        <param name="lambda_value"   value="1000000" />
-        <output name="oname"         file="ST000006_log_and_glog_transformation_glog_lambda_1000000.tsv" />
-      </test>
-    </tests>
-
-'
-
-
-
-
+echo "### Finished test: ${TEST} on $(date)"
