bdb246d0887718b705a5a3e978c1eadf551bdcd0
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-01-28 15:34:01 -0500

    Update modify_design_file test script to pass red/green test

--- tests/modify_design_file.sh
+++ tests/modify_design_file.sh
@@ -1,35 +1,30 @@
 #!/bin/bash
-TIMESTAMP=$(date +%s)
-SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+#     <test>
+#        <param name="input"  value="ST000006_data.tsv"/>
+#        <param name="design" value="ST000006_design_group_name_underscore.tsv"/>
+#        <param name="uniqID" value="Retention_Index" />
+#        <param name="group"  value="White_wine_type_and_source_nosp" />
+#        <param name="toDrop" value="Chardonnay_Napa_CA2003,Riesling_CA2004,SauvignonBlanc_LakeCounty_CA2001" />
+#        <output name="out"   file="ST000006_modify_design_file_output.tsv" />
+#     </test>
 
-CODEDIR="${SCRIPT_DIR}/.."
-INPUTDIR="${SCRIPT_DIR}/../test-data"
-OUTPUTDIR="$(pwd)/output_${TIMESTAMP}"
-mkdir -p ${OUTPUTDIR}
+SCRIPT=$(basename "${BASH_SOURCE[0]}");
+TEST="${SCRIPT%.*}"
+TESTDIR="testout/${TEST}"
+INPUT_DIR="galaxy/test-data"
+OUTPUT_DIR=$TESTDIR
+rm -rf "${TESTDIR}"
+mkdir -p "${TESTDIR}"
+echo "### Starting test: ${TEST}"
+if [[ $# -gt 0 ]]; then OUTPUT_DIR=$1 ; fi
+mkdir -p "${OUTPUT_DIR}"
 
-/modify_design_file.py \
-		-i  $INPUTDIR/ST000006_data.tsv \
-		-d  $INPUTDIR/ST000006_design_group_name_underscore.tsv \
-		-id Retention_Index \
-                -g White_wine_type_and_source_nosp \
-                -dp "Chardonnay_Napa_CA2003,Riesling_CA2004,SauvignonBlanc_LakeCounty_CA2001" \
-         	-o  $OUTPUTDIR/ST000006_modify_design_file_output.tsv \
+modify_design_file.py \
+    -i  "$INPUT_DIR/ST000006_data.tsv" \
+    -d  "$INPUT_DIR/ST000006_design_group_name_underscore.tsv" \
+    -id Retention_Index \
+    -g White_wine_type_and_source_nosp \
+    -dp "Chardonnay_Napa_CA2003,Riesling_CA2004,SauvignonBlanc_LakeCounty_CA2001" \
+    -o  "$OUTPUT_DIR/ST000006_modify_design_file_output.tsv"
 
-
-: '
-
-The piece of code below is the test for the xml file.
-
-    <tests>
-     <test>
-        <param name="input"  value="ST000006_data.tsv"/>
-        <param name="design" value="ST000006_design_group_name_underscore.tsv"/>
-        <param name="uniqID" value="Retention_Index" />
-        <param name="group"  value="White_wine_type_and_source_nosp" />
-        <param name="toDrop" value="Chardonnay_Napa_CA2003,Riesling_CA2004,SauvignonBlanc_LakeCounty_CA2001" />
-        <output name="out"   file="ST000006_modify_design_file_output.tsv" />
-     </test>
-    </tests>
-
-'
-
+echo "### Finished test: ${TEST} on $(date)"
