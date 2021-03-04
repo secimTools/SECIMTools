219bb66b83b79635c64bde57b42eabf7d2b144e2
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-03-04 13:42:28 -0500

    Remove superfluous print statements

--- src/scripts/modify_design_file.py
+++ src/scripts/modify_design_file.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 ################################################################################
 # DATE: 2016/October/10
-# 
+#
 # MODULE: subset_data.py
 #
 # VERSION: 1.2
-# 
-# AUTHOR: Miguel Ibarra (miguelib@ufl.edu) 
 #
-# DESCRIPTION: Subsets design file data based on the groups in the 
-#               design file. 
+# AUTHOR: Miguel Ibarra (miguelib@ufl.edu)
+#
+# DESCRIPTION: Subsets design file data based on the groups in the
+#               design file.
 #
 ################################################################################
 
@@ -33,27 +33,27 @@ def getOptions():
     parser = argparse.ArgumentParser(description="Removes samples from the design file" \
                                     "belonging to the user-specified group(s).")
     # Standar Input
-    standar = parser.add_argument_group(title="Standard input", 
+    standar = parser.add_argument_group(title="Standard input",
                         description= "Standard input for SECIM tools.")
-    standar.add_argument("-i","--input",dest="input", action='store', 
+    standar.add_argument("-i","--input",dest="input", action='store',
                         required=True, help="Input dataset in wide format.")
-    standar.add_argument("-d","--design",dest="design", action='store', 
+    standar.add_argument("-d","--design",dest="design", action='store',
                         required=True, help="Design file.")
     standar.add_argument("-id","--uniqID",dest="uniqID",action="store",
                         required=True, help="Name of the column with unique" \
                         "dentifiers.")
-    standar.add_argument("-g","--group", dest="group", action='store', 
+    standar.add_argument("-g","--group", dest="group", action='store',
                         required=False, help="Name of column in design file" \
                         "with Group/treatment information.")
     # Tool Especific
-    tool = parser.add_argument_group(title="Tool specific input", 
+    tool = parser.add_argument_group(title="Tool specific input",
                         description= "Input that is especific for this tool.")
-    tool.add_argument("-dp","--drops", dest="drops", action='store', 
+    tool.add_argument("-dp","--drops", dest="drops", action='store',
                         required=True, help="Name of the groups in your"\
                         "group/treatment column that you want to remove from the design file.")
 
     # Output Paths
-    output = parser.add_argument_group(title='Output paths', 
+    output = parser.add_argument_group(title='Output paths',
                         description="Paths for the output files")
     output.add_argument("-o","--out",dest="out",action="store",
                         required=True,help="Output path for the new design file")
@@ -110,7 +110,7 @@ def cleanStr(x):
 def main(args):
     # Importing data trough
     logger.info("Importing data through wideToDesign data manager")
-    dat = wideToDesign(args.input, args.design, args.uniqID, 
+    dat = wideToDesign(args.input, args.design, args.uniqID,
                         logger=logger)
 
     # Cleaning from missing data
@@ -119,8 +119,6 @@ def main(args):
     # Making sure all the groups to drop actually exist on the design column
     if args.group:
         for todrop in args.drops:
-            print("design file: ")
-            print(dat.design)
             if todrop in list(set(dat.design[args.group].values)):
                 pass
             else:
@@ -128,7 +126,7 @@ def main(args):
                             "of your design file".format(todrop,args.group))
                 raise ValueError
 
-    # If the subsetting is going to be made by group the select de sampleIDs 
+    # If the subsetting is going to be made by group the select de sampleIDs
     # from the design file
     logger.info(u"Getting sampleNames to drop")
     if args.group:
