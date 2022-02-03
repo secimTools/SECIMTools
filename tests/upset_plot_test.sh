9b2b83dcd8fa47a80f9fc99648f2bbae30c4af65
Carter Johnson <carter.johnson@ufgi-serrata.ahc.ufl.edu>
2022-02-03 12:23:15 -0500

    Added shell script and example design file for upset_plot.py

---
+++ tests/upset_plot_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+## use SECIMTools conda env
+export PATH=/ufgi-serrata/data/cjohnson/cj_conda_envs/__secimtools@21.2.21/bin:$PATH
+
+PROJ=/ufgi-serrata/data/cjohnson/Upset_testing
+SCRIPT=/ufgi-serrata/data/cjohnson/SECIMTools/src/scripts
+
+#INPUT=~/mclab/SHARE/McIntyre_Lab/Transcript_distances/rerun_analysis/MaizeWang2020/test_trand_chr10/TranD_2GTF_B73_R_vs_B73_END_chr10
+
+
+python $SCRIPT/upset_plot.py \
+    --wide ~/mclab/SHARE/McIntyre_Lab/Transcript_distances/rerun_analysis/MaizeWang2020/test_trand_chr10/TranD_2GTF_B73_R_vs_B73_END_chr10/pairwise_transcript_distance.csv \
+    --uniqID rowID \
+    --title upset_test \
+    --design $PROJ/upset_plot_test_design.csv \
+    --outD $PROJ
+
